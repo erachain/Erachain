6e504572771e9d2dd350c988d135e1b1f7570263
Саша <rty@yandex.ru>
2016-04-20 18:10:37 +0900

    Lang ok

--- ERM/src/lang/Lang.java
+++ ERM/src/lang/Lang.java
@@ -18,7 +18,7 @@ import settings.Settings;
 
 public class Lang {
 												
-	public static final String translationsUrl = "https://raw.githubusercontent.com/icreator/ERMbase/master/";
+	public static final String translationsUrl = "https://raw.githubusercontent.com/icreator/ERMbase/master/ERM/languages/";
 	
 	private static final Logger LOGGER = Logger.getLogger(Lang.class);
 	private static Lang instance;
