509b5703d555bf332d336340f95af2c295421611
Саша <rty@yandex.ru>
2016-09-14 13:24:52 +0900

    translate filechooser +

--- ERM/src/gui/library/My_JFileChooser.java
+++ ERM/src/gui/library/My_JFileChooser.java
@@ -36,9 +36,65 @@ public class My_JFileChooser extends JFileChooser{
 		UIManager.put("FileChooser.fileTypeHeaderText",  Lang.getInstance().translate("Type"));
 		UIManager.put("FileChooser.fileDateHeaderText",  Lang.getInstance().translate("File Date"));
 		UIManager.put("FileChooser.fileAttrHeaderText",  Lang.getInstance().translate("File Attr"));
+		
 
 		UIManager.put("FileChooser.acceptAllFileFilterText",  Lang.getInstance().translate("All Files"));
 		this.updateUI();
+		
+		/*
+		      FileChooser.acceptAllFileFilterText=Все файлы
+    FileChooser.cancelButtonText=Отмена
+    FileChooser.cancelButtonToolTipText=Отмена
+    FileChooser.deleteFileButtonText=Удалить
+    FileChooser.deleteFileButtonToolTipText=Удалить файл
+    FileChooser.detailsViewButtonAccessibleName=Подробно
+    FileChooser.detailsViewButtonToolTipText=Подробно
+    FileChooser.directoryDescriptionText=Папка
+    FileChooser.directoryOpenButtonText=Открыть
+    FileChooser.directoryOpenButtonToolTipText=Открыть
+    FileChooser.enterFilenameLabelText=Имя
+    FileChooser.fileDescriptionText=Описание
+    FileChooser.fileNameLabelText=Имя файла
+    FileChooser.filesLabelText=Файлы
+    FileChooser.filesOfTypeLabelText=Типы файлов
+    FileChooser.filterLabelText=Тип(ы) файла
+    FileChooser.foldersLabelText=Папка
+    FileChooser.helpButtonText=Помощь
+    FileChooser.helpButtonToolTipText=Помощь
+    FileChooser.homeFolderAccessibleName=Дом
+    FileChooser.homeFolderToolTipText=Дом
+    FileChooser.listViewButtonAccessibleName=Список
+    FileChooser.listViewButtonToolTipText=Список
+    FileChooser.lookInLabelText=Католог:
+    FileChooser.newFolderAccessibleName=Создать папку
+    FileChooser.newFolderButtonText=Создать папку
+    FileChooser.newFolderButtonToolTipText=Создать папку
+    FileChooser.newFolderDialogText=Создать папку
+    FileChooser.newFolderErrorSeparator=Ошибка создания
+    FileChooser.newFolderErrorText=Ошибка создания
+    FileChooser.newFolderToolTipText=Создать папку
+    FileChooser.openButtonText=Открыть
+    FileChooser.openButtonToolTipText=Открыть
+    FileChooser.openDialogTitleText=Открыть
+    FileChooser.other.newFolder=Создать папку
+    FileChooser.other.newFolder.subsequent=Создать папку
+    FileChooser.win32.newFolder=Создать папку
+    FileChooser.win32.newFolder.subsequent=Создать папку
+    FileChooser.pathLabelText=Путь
+    FileChooser.renameFileButtonText=Переименовать
+    FileChooser.renameFileButtonToolTipText=Переименовать
+    FileChooser.renameFileDialogText=Переименовать
+    FileChooser.renameFileErrorText=Ошибка переименования
+    FileChooser.renameFileErrorTitle=Ошибка переименования
+    FileChooser.saveButtonText=Сохранить
+    FileChooser.saveButtonToolTipText=Сохранить
+    FileChooser.saveDialogTitleText=Сохранить
+    FileChooser.saveInLabelText=Католог:
+    FileChooser.updateButtonText=Обновить
+    FileChooser.updateButtonToolTipText=Обновить
+    FileChooser.upFolderAccessibleName=Вверх
+    FileChooser.upFolderToolTipText=Вверх
+		 */
 
 		} 
 
