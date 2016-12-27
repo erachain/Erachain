37d72ec5adeed48454939db8224776ffec1adf9f
саша <@10.0.0.104>
2016-12-27 04:08:03 +0300

    =

--- ERM/src/gui/library/MTextPane.java
+++ ERM/src/gui/library/MTextPane.java
@@ -15,14 +15,15 @@ public class MTextPane extends JTextPane{
 		super();
 		setContentType("text/html");
 		str =  Processor.process(str);
+		int font_saze = UIManager.getFont("Label.font").getSize();
 		str= "<head><style>"
-				+ " h1{ font-size: 24px;  } "
-				+ " h2{ font-size: 24px;  }"
-				+ " h3{ font-size: 24px;  }"
-				+ " h4{ font-size: 24px;  }"
-				+ " h5{ font-size: 24px;  }"
+				+ " h1{ font-size: "+ font_saze +"px;  } "
+				+ " h2{ font-size: "+ font_saze +"px;  }"
+				+ " h3{ font-size: "+ font_saze +"px;  }"
+				+ " h4{ font-size: "+ font_saze +"px;  }"
+				+ " h5{ font-size: "+ font_saze +"px;  }"
 				+ " body{ font-family:"+UIManager.getFont("Label.font").getFamily()
-				+  "; font-size:"+ UIManager.getFont("Label.font").getSize() +"px}"
+				+  "; font-size:"+ font_saze +"px}"
 				+ "</style> </head><body>"
 				+str +"</body>";
 		this.setText(str);
