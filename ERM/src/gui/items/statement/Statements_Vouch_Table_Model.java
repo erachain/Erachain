8b5699a5f66d79b5ee633317cd8c14ce5ae3a9c9
Саша <rty@yandex.ru>
2017-02-18 19:34:59 +1000

    vouch +++++

--- ERM/src/gui/items/statement/Statements_Vouch_Table_Model.java
+++ ERM/src/gui/items/statement/Statements_Vouch_Table_Model.java
@@ -46,249 +46,230 @@ public class Statements_Vouch_Table_Model extends AbstractTableModel implements 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
-
 	public static final int COLUMN_TIMESTAMP = 0;
-//	public static final int COLUMN_TYPE = 1;
+	// public static final int COLUMN_TYPE = 1;
 	public static final int COLUMN_CREATOR = 1;
-//	public static final int COLUMN_BODY = 2;
-//	public static final int COLUMN_AMOUNT = 2;
-//	public static final int COLUMN_FEE = 3;
+	// public static final int COLUMN_BODY = 2;
+	// public static final int COLUMN_AMOUNT = 2;
+	// public static final int COLUMN_FEE = 3;
 	List<Transaction> transactions;
-	
-//	private SortableList<byte[], Transaction> transactions;
-	
-	private String[] columnNames = Lang.getInstance().translate(new String[]{"Timestamp", "Signatures"});//, AssetCls.FEE_NAME});
-	private Boolean[] column_AutuHeight = new Boolean[]{true,true};
-//	private Map<byte[], BlockingQueue<Block>> blocks;
-	//private Transaction transaction;
+
+	// private SortableList<byte[], Transaction> transactions;
+
+	private String[] columnNames = Lang.getInstance().translate(new String[] { "Timestamp", "Signatures" });// ,
+																											// AssetCls.FEE_NAME});
+	private Boolean[] column_AutuHeight = new Boolean[] { true, true };
+	// private Map<byte[], BlockingQueue<Block>> blocks;
+	// private Transaction transaction;
 	private int blockNo;
 	private int recNo;
 
 	TransactionFinalMap table;
 
 	private ObserverMessage message;
-	
-	
-	
-	public Statements_Vouch_Table_Model(Transaction transaction){
+
+	private String sss;
+
+	public Statements_Vouch_Table_Model(Transaction transaction) {
 		table = DBSet.getInstance().getTransactionFinalMap();
 		blockNo = transaction.getBlockHeight(DBSet.getInstance());
 		recNo = transaction.getSeqNo(DBSet.getInstance());
 		transactions = new ArrayList<Transaction>();
-	//	transactions = read_Sign_Accoutns();
-		DBSet.getInstance().getVouchRecordMap().addObserver(this);	
+		// transactions = read_Sign_Accoutns();
+		DBSet.getInstance().getTransactionFinalMap().addObserver(this);
+		DBSet.getInstance().getVouchRecordMap().addObserver(this);
 
-	
-	
 	}
-	
-	
-	
-	
 
-		public Class<? extends Object> getColumnClass(int c) {     // set column type
-			Object o = getValueAt(0, c);
-			return o==null?null:o.getClass();
-			   }
-	
-		// читаем колонки которые изменяем высоту	   
-		public Boolean[] get_Column_AutoHeight(){
-			
-			return this.column_AutuHeight;
-		}
-	// устанавливаем колонки которым изменить высоту	
-		public void set_get_Column_AutoHeight( Boolean[] arg0){
-			this.column_AutuHeight = arg0;	
-		}
+	public Class<? extends Object> getColumnClass(int c) { // set column type
+		Object o = getValueAt(0, c);
+		return o == null ? null : o.getClass();
+	}
+
+	// читаем колонки которые изменяем высоту
+	public Boolean[] get_Column_AutoHeight() {
+
+		return this.column_AutuHeight;
+	}
+
+	// устанавливаем колонки которым изменить высоту
+	public void set_get_Column_AutoHeight(Boolean[] arg0) {
+		this.column_AutuHeight = arg0;
+	}
+
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
 		return this.columnNames.length;
 	}
-	
+
 	@Override
-	public String getColumnName(int index) 
-	{
+	public String getColumnName(int index) {
 		return this.columnNames[index];
 	}
 
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
-		if (transactions ==null) return 0;
-		int c =0;
-		for (Transaction a:this.transactions){
-		if(a != null)++c;
+		if (transactions == null)
+			return 0;
+		int c = 0;
+		for (Transaction a : this.transactions) {
+			if (a != null)
+				++c;
 		}
-		return c; //transactions.size();
+		return c; // transactions.size();
 	}
-	
 
 	@Override
 	public Object getValueAt(int row, int column) {
 		// TODO Auto-generated method stub
-		try
-		{
-			if(this.transactions == null || this.transactions.size() -1 < row)
-			{
+		try {
+			if (this.transactions == null || this.transactions.size() - 1 < row) {
 				return null;
 			}
-			
+
 			Transaction transaction = this.transactions.get(row);
-			
-			
-	//		R_Vouch i;
-			switch(column)
-			{
+
+			// R_Vouch i;
+			switch (column) {
 			case COLUMN_TIMESTAMP:
-				
-				//return DateTimeFormat.timestamptoString(transaction.getTimestamp()) + " " + transaction.getTimestamp();
-				return transaction.viewTimestamp(); // + " " + transaction.getTimestamp() / 1000;
-				
-		/*	case COLUMN_TYPE:
-				
-				//return Lang.transactionTypes[transaction.getType()];
-				return Lang.getInstance().translate(transaction.viewTypeName());
-		*/
-				
-				
-				
-	//		case	 COLUMN_BODY:
-				
-				
-				
-	//			 i = (R_Vouch)transaction;
-				
-	//			return new String( i.getData(), Charset.forName("UTF-8") ) ;//transaction.viewReference();//.viewProperies();
-				
-				
-	//		case COLUMN_AMOUNT:
-				
-	//			return NumberAsString.getInstance().numberAsString(transaction.getAmount(transaction.getCreator()));
-				
-	//		case COLUMN_FEE:
-				
-	//			return NumberAsString.getInstance().numberAsString(transaction.getFee());	
-			
+
+				// return
+				// DateTimeFormat.timestamptoString(transaction.getTimestamp())
+				// + " " + transaction.getTimestamp();
+				return transaction.viewTimestamp(); // + " " +
+													// transaction.getTimestamp()
+													// / 1000;
+
+			/*
+			 * case COLUMN_TYPE:
+			 * 
+			 * //return Lang.transactionTypes[transaction.getType()]; return
+			 * Lang.getInstance().translate(transaction.viewTypeName());
+			 */
+
+			// case COLUMN_BODY:
+
+			// i = (R_Vouch)transaction;
+
+			// return new String( i.getData(), Charset.forName("UTF-8") )
+			// ;//transaction.viewReference();//.viewProperies();
+
+			// case COLUMN_AMOUNT:
+
+			// return
+			// NumberAsString.getInstance().numberAsString(transaction.getAmount(transaction.getCreator()));
+
+			// case COLUMN_FEE:
+
+			// return
+			// NumberAsString.getInstance().numberAsString(transaction.getFee());
+
 			case COLUMN_CREATOR:
-				
-				
+
 				return (transaction.getCreator().toString());
 			}
-			
+
 			return null;
-			
+
 		} catch (Exception e) {
-	//		LOGGER.error(e.getMessage(),e);
+			// LOGGER.error(e.getMessage(),e);
 			return null;
 		}
 	}
-	
-	
+
 	@Override
-	public void update(Observable o, Object arg) 
-	{	
-		//try
-		//{
-			this.syncUpdate(o, arg);
-		//}
-	//	catch(Exception e)
-	//	{
-			//GUI ERROR
-	//	}
+	public void update(Observable o, Object arg) {
+		// try
+		// {
+		this.syncUpdate(o, arg);
+		// }
+		// catch(Exception e)
+		// {
+		// GUI ERROR
+		// }
 	}
-	
-	public synchronized void syncUpdate(Observable o, Object arg)
-	{
+
+	public synchronized void syncUpdate(Observable o, Object arg) {
 		message = (ObserverMessage) arg;
-//		System.out.println( message.getType());
-		
-		//CHECK IF NEW LIST
-		if(message.getType() == ObserverMessage.LIST_VOUCH_TYPE)
-		{
-			if(this.transactions.size() == 0)
-			{
-			transactions = read_Sign_Accoutns();
-			this.fireTableDataChanged();
+		// System.out.println( message.getType());
+
+		// CHECK IF NEW LIST
+		if (message.getType() == ObserverMessage.LIST_VOUCH_TYPE) {
+			if (this.transactions.size() == 0) {
+				transactions = read_Sign_Accoutns();
+				this.fireTableDataChanged();
 			}
-			
+
+		}
+
+		// CHECK IF LIST UPDATED
+		if (message.getType() == ObserverMessage.ADD_TRANSACTION_TYPE
+		// || message.getType() == ObserverMessage.REMOVE_VOUCH_TYPE
+		// || message.getType() == ObserverMessage.LIST_STATEMENT_TYPE
+		// || message.getType() == ObserverMessage.REMOVE_STATEMENT_TYPE
+
+		) {
+			Transaction ss = (Transaction) message.getValue();
+			if (ss.getType() == Transaction.VOUCH_TRANSACTION) {
+				R_Vouch ss1 = (R_Vouch) ss;
+				if (ss1.getVouchHeight() == blockNo && ss1.getSeqNo(DBSet.getInstance()) == recNo)
+
+					if (!this.transactions.contains(ss)){
+						this.transactions.add(ss);
+						this.fireTableDataChanged();
+					}
+			}
+
 			
 		}
-		
-		
-		//CHECK IF LIST UPDATED
-		if( 	message.getType() == ObserverMessage.ADD_VOUCH_TYPE 
-		//		|| message.getType() == ObserverMessage.REMOVE_VOUCH_TYPE
-		//		|| message.getType() == ObserverMessage.LIST_STATEMENT_TYPE
-		//		|| message.getType() == ObserverMessage.REMOVE_STATEMENT_TYPE
-				
-				)
-		{
-			//this.statuses = (TreeMap<Long, Stack<Tuple5<Long, Long, byte[], Integer, Integer>>>) message.getValue();
-		//	transactions.clear();
-			@SuppressWarnings("unchecked")
-			Tuple2<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>> a = (Tuple2<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>) message.getValue();
-			if (a.a.a == blockNo && a.a.b == recNo){
-				List<Tuple2<Integer, Integer>> ff = a.b.b;
-				
-				for (Tuple2<Integer, Integer> ll:ff){
-				Integer bl = ll.a;
-				Integer seg = ll.b;
-				
-				Transaction kk = table.getTransaction(bl, seg);
-				if (!this.transactions.contains(kk))	 this.transactions.add(kk);
-				}
-			
-			}
-			
-		//	transactions = read_Sign_Accoutns();
-			this.fireTableDataChanged();
-		}	
-	}	
-	
-	
-	private List<Transaction> read_Sign_Accoutns(){
+	}
+
+	private List<Transaction> read_Sign_Accoutns() {
 		List<Transaction> tran = new ArrayList<Transaction>();
-		//ArrayList<Transaction> db_transactions;
-		//db_transactions = new ArrayList<Transaction>();
-		//tran = new ArrayList<Transaction>();
-		// база данных	
-		//DBSet dbSet = DBSet.getInstance();
-	
+		// ArrayList<Transaction> db_transactions;
+		// db_transactions = new ArrayList<Transaction>();
+		// tran = new ArrayList<Transaction>();
+		// база данных
+		// DBSet dbSet = DBSet.getInstance();
+
 		/*
-		Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>> signs = DBSet.getInstance().getVouchRecordMap().get(blockNo, recNo);
-		
-		
-		if (signs == null) return null;
-		for(Tuple2<Integer, Integer> seq: signs.b)
-		{
-			
-			Transaction kk = table.getTransaction(seq.a, seq.b);
-			if (!tran.contains(kk))	 tran.add(kk);
-		}
-		*/
-		
-      @SuppressWarnings("unchecked")
-	SortableList<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>> rec = (SortableList<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>) message.getValue();
-		
-		Iterator<Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>> ss = rec.iterator();
-		while(ss.hasNext()){
-		Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>> a = (Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>) ss.next();
-		// block
-		if (a.getA().a == blockNo && a.getA().b == recNo){
-			List<Tuple2<Integer, Integer>> ff = a.getB().b;
-			
-			for (Tuple2<Integer, Integer> ll:ff){
-			Integer bl = ll.a;
-			Integer seg = ll.b;
-			
-			Transaction kk = table.getTransaction(bl, seg);
-			if (!tran.contains(kk))	 tran.add(kk);
+		 * Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>> signs =
+		 * DBSet.getInstance().getVouchRecordMap().get(blockNo, recNo);
+		 * 
+		 * 
+		 * if (signs == null) return null; for(Tuple2<Integer, Integer> seq:
+		 * signs.b) {
+		 * 
+		 * Transaction kk = table.getTransaction(seq.a, seq.b); if
+		 * (!tran.contains(kk)) tran.add(kk); }
+		 */
+
+		@SuppressWarnings("unchecked")
+		SortableList<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>> rec = (SortableList<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>) message
+				.getValue();
+
+		Iterator<Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>> ss = rec
+				.iterator();
+		while (ss.hasNext()) {
+			Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>> a = (Pair<Tuple2<Integer, Integer>, Tuple2<BigDecimal, List<Tuple2<Integer, Integer>>>>) ss
+					.next();
+			// block
+			if (a.getA().a == blockNo && a.getA().b == recNo) {
+				List<Tuple2<Integer, Integer>> ff = a.getB().b;
+
+				for (Tuple2<Integer, Integer> ll : ff) {
+					Integer bl = ll.a;
+					Integer seg = ll.b;
+
+					Transaction kk = table.getTransaction(bl, seg);
+					if (!tran.contains(kk))
+						tran.add(kk);
+				}
 			}
+
 		}
-		
-	}	
 		return tran;
 	}
 
