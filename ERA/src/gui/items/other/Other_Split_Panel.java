b60c2e41dc45b567744c78a46d624e5dabd5add4
Саша <rty@yandex.ru>
2017-06-11 15:45:33 +1000

    menu from peers

--- ERA/src/gui/items/other/Other_Split_Panel.java
+++ ERA/src/gui/items/other/Other_Split_Panel.java
@@ -3,12 +3,17 @@ package gui.items.other;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Map;
 import java.util.TreeMap;
 
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 
 import gui.CoreRowSorter;
@@ -18,6 +23,7 @@ import gui.models.BlocksTableModel;
 import gui.models.PeersTableModel;
 import gui.models.WalletBlocksTableModel;
 import lang.Lang;
+import utils.TableMenuPopupUtil;
 
 public class Other_Split_Panel extends Split_Panel{
 
@@ -31,6 +37,7 @@ public class Other_Split_Panel extends Split_Panel{
 	private MTable jTable_All_Block;
 	private WalletBlocksTableModel blocksModel;
 	private MTable jTable_My_Block;
+	private JPopupMenu peers_Menu;
 
 	public Other_Split_Panel() {
 		super("Other_Split_Panel");
@@ -40,9 +47,85 @@ public class Other_Split_Panel extends Split_Panel{
          this.jTable_Peers = new MTable(this.peersTableModel);
          this.jTable_Peers.setAutoCreateRowSorter(true);	
   
+
+         peers_Menu = new JPopupMenu();
+         JMenuItem Baned_2_Min_Item = new JMenuItem(Lang.getInstance().translate("Ban in 10 min."));
+         Baned_2_Min_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(Baned_2_Min_Item);
+         JMenuItem Baned_10_Min_Item = new JMenuItem(Lang.getInstance().translate("Ban in 60 min."));
+         Baned_10_Min_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(Baned_10_Min_Item);
+         JMenuItem Baned_60_Min_Item = new JMenuItem(Lang.getInstance().translate("Ban in 3 hours"));
+         Baned_60_Min_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(Baned_60_Min_Item);
+         JMenuItem Baned_24_Hours_Item = new JMenuItem(Lang.getInstance().translate("Ban in 24 hours"));
+         Baned_24_Hours_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(Baned_24_Hours_Item);
+
+         JMenuItem Baned_Allways_Item = new JMenuItem(Lang.getInstance().translate("Ban Allways"));
+         Baned_Allways_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(Baned_Allways_Item);
+         peers_Menu.add(new JSeparator());
+         JMenuItem allow_Item = new JMenuItem(Lang.getInstance().translate("Allow"));
+         allow_Item.addActionListener(new ActionListener(){
+
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				// TODO Auto-generated method stub
+ 				
+ 			}
+       	
+         });
+         peers_Menu.add(allow_Item);
+         
+         TableMenuPopupUtil.installContextMenu( this.jTable_Peers, peers_Menu);
+         
+         
+         
+         
          this.toolBar_LeftPanel.setVisible(false);
          this.jToolBar_RightPanel.setVisible(false);
-         this.jTable_Peers.setEnabled(false);
+       //  this.jTable_Peers.setEnabled(false);
          
          jPanel2 = new JPanel();
         
