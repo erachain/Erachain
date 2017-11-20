380317e77744f9616aae60450a484e81803c9452
adm <adm@icreator>
2017-11-20 10:56:02 +0300

    new GENESIS

--- ERA/src/core/BlockChain.java
+++ ERA/src/core/BlockChain.java
@@ -91,7 +91,7 @@ public class BlockChain
 	//TESTNET 
 												//   1486444444444l
 										   //	 1487844444444   1509434273     1509434273 
-	public static final long DEFAULT_MAINNET_STAMP = DEVELOP_USE?1501251005000l:1487844793333l;
+	public static final long DEFAULT_MAINNET_STAMP = DEVELOP_USE?1511164500000l:1487844793333l;
 
 	//public static final int FEE_MIN_BYTES = 200;
 	public static final int FEE_PER_BYTE = 64;
