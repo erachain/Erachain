8e740dab00126d54078414f746ba4c75074464f5
adm <adm@icreator>
2017-11-29 22:19:00 +0300

    dddd

--- ERA/src/controller/Controller.java
+++ ERA/src/controller/Controller.java
@@ -108,7 +108,7 @@ public class Controller extends Observable {
 	// IF new abilities is made - new license insert in CHAIN and set this KEY
 	public static final long LICENSE_KEY = 1014l;
 	public static final String APP_NAME = BlockChain.DEVELOP_USE?"Erachain-dev":"Erachain";
-	private static final String version = "4.0.02 alpha";
+	private static final String version = "4.1.01 alpha";
 	private static final String buildTime = "2017-11-19 15:33:33 UTC";
 	private static long buildTimestamp;
 	
