184951e1e69d89bc5f438936a87e89fb2b4e7362
Саша <rty@yandex.ru>
2017-12-05 11:28:53 +1000

    Gui Person +

--- ERA/src/gui/items/persons/Person_Vouch_From_Table_Model.java
+++ ERA/src/gui/items/persons/Person_Vouch_From_Table_Model.java
@@ -1,44 +1,21 @@
 package gui.items.persons;
 
-import java.math.BigDecimal;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Stack;
-import java.util.TreeMap;
-import java.util.concurrent.BlockingQueue;
-
 import javax.swing.table.AbstractTableModel;
 import javax.validation.constraints.Null;
-
-import org.json.simple.JSONArray;
+import org.apache.log4j.Logger;
 import org.mapdb.Fun.Tuple2;
-import org.mapdb.Fun.Tuple5;
-
-import controller.Controller;
-import core.BlockChain;
-import core.account.Account;
 import core.account.PublicKeyAccount;
-import core.block.Block;
-import core.block.GenesisBlock;
-import core.item.assets.AssetCls;
 import core.item.persons.PersonCls;
 import core.transaction.R_SertifyPubKeys;
-import core.transaction.R_SignNote;
-//import core.transaction.R_SignStatement_old;
-import core.transaction.R_Vouch;
 import core.transaction.Transaction;
-import datachain.DCMap;
 import datachain.DCSet;
 import datachain.SortableList;
 import datachain.TransactionFinalMap;
-import datachain.TransactionMap;
 import lang.Lang;
 import utils.DateTimeFormat;
 import utils.ObserverMessage;
@@ -52,39 +29,23 @@ public class Person_Vouch_From_Table_Model extends AbstractTableModel implements
 	private static final long serialVersionUID = 1L;
 
 	public static final int COLUMN_TIMESTAMP = 0;
-	// public static final int COLUMN_TYPE = 1;
 	public static final int COLUMN_CREATOR = 1;
-	// public static final int COLUMN_BODY = 2;
-	// public static final int COLUMN_AMOUNT = 2;
-	 public static final int COLUMN_HEIGHT = 2;
-	
-	 
-	 //public static final int COLUMN_CREATOR_NAME =30;
+	public static final int COLUMN_HEIGHT = 2;
 	List<R_SertifyPubKeys> transactions;
-
-	// private SortableList<byte[], Transaction> transactions;
-
 	private String[] columnNames = Lang.getInstance().translate(new String[] { "Timestamp", "Persons", "Height" });// ,
-																											// AssetCls.FEE_NAME});
 	private Boolean[] column_AutuHeight = new Boolean[] { true, true };
 	PersonCls person;
-
 	TransactionFinalMap table;
-
 	private ObserverMessage message;
-
-	private String sss;
-
 	private boolean fire;
-
+	private static final Logger LOGGER = Logger.getLogger(Person_Vouch_From_Table_Model.class);
+	
+	
 	public Person_Vouch_From_Table_Model(PersonCls person) {
 		fire = false;
 		this.person=person;
 		transactions = new ArrayList<R_SertifyPubKeys>();
-		// transactions = read_Sign_Accoutns();
 		addObservers();
-		
-
 	}
 
 	public Class<? extends Object> getColumnClass(int c) { // set column type
@@ -184,31 +145,33 @@ public class Person_Vouch_From_Table_Model extends AbstractTableModel implements
 			return null;
 
 		} catch (Exception e) {
-			// LOGGER.error(e.getMessage(),e);
+			LOGGER.error(e.getMessage(),e);
 			return null;
 		}
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// try
-		// {
+		 try
+		 {
 		this.syncUpdate(o, arg);
-		// }
-		// catch(Exception e)
-		// {
+		 }
+		 catch(Exception e)
+		 {
 		// GUI ERROR
-		// }
+		 LOGGER.error(e.getMessage(),e);
+		 }
 	}
 
 	public synchronized void syncUpdate(Observable o, Object arg) {
 		message = (ObserverMessage) arg;
 
 		
-		if(message.getType() == ObserverMessage.WALLET_LIST_TRANSACTION_TYPE)
+		if(message.getType() == ObserverMessage.LIST_TRANSACTION_TYPE)
 		{
 			//CHECK IF NEW LIST
 		
+			@SuppressWarnings("unchecked")
 			SortableList<byte[], Transaction> ss = (SortableList<byte[], Transaction>) message.getValue();
 			Iterator<Pair<byte[], Transaction>> s = ss.iterator();
 			if (this.transactions.size() == 0){
@@ -232,11 +195,7 @@ public class Person_Vouch_From_Table_Model extends AbstractTableModel implements
 				this.fireTableDataChanged();
 			fire = true;
 			}
-		} else if (message.getType() == ObserverMessage.WALLET_ADD_TRANSACTION_TYPE
-		// || message.getType() == ObserverMessage.REMOVE_VOUCH_TYPE
-		// || message.getType() == ObserverMessage.LIST_STATEMENT_TYPE
-		// || message.getType() == ObserverMessage.REMOVE_STATEMENT_TYPE
-				) {
+		} else if (message.getType() == ObserverMessage.ADD_TRANSACTION_TYPE) {
 			Transaction ss = (Transaction) message.getValue();
 			if (ss.getType() == Transaction.CERTIFY_PUB_KEYS_TRANSACTION) {
 				R_SertifyPubKeys ss1 = (R_SertifyPubKeys) ss;
@@ -248,24 +207,30 @@ public class Person_Vouch_From_Table_Model extends AbstractTableModel implements
 					}
 				}
 			}
+		} else if (message.getType() == ObserverMessage.REMOVE_TRANSACTION_TYPE) {
+			Transaction ss = (Transaction) message.getValue();
+			if (ss.getType() == Transaction.CERTIFY_PUB_KEYS_TRANSACTION) {
+				R_SertifyPubKeys ss1 = (R_SertifyPubKeys) ss;
+				Tuple2<Integer, PersonCls> personRes = ss1.getCreator().getPerson();
+				if (personRes != null && personRes.b.getKey() == person.getKey()) {
+					if (this.transactions.contains(ss1)){
+						this.transactions.remove(ss1);
+						this.fireTableDataChanged();
+					}
+				}
+			}
+			
 		}
+		
 	}
 
 public void addObservers(){
-		
-//	DCSet.getInstance().getTransactionFinalMap().addObserver(this);
-	DCSet.getInstance().getTransactionMap().addObserver(this);
-	
-		
+	DCSet.getInstance().getTransactionFinalMap().addObserver(this);
 	}
-	
 	
 	public void removeObservers() 
 	{
-		
-		
 		DCSet.getInstance().getTransactionFinalMap().deleteObserver(this);
-		DCSet.getInstance().getTransactionMap().deleteObserver(this);
 	}
 
 }
