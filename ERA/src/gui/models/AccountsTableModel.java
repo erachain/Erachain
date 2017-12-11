965182d685397e28e9f0f273eb9d5dc7daaca93c
adm <adm@icreator>
2017-12-12 00:37:06 +0300

    fix

--- ERA/src/gui/models/AccountsTableModel.java
+++ ERA/src/gui/models/AccountsTableModel.java
@@ -119,7 +119,7 @@ public class AccountsTableModel extends AbstractTableModel implements Observer
 		case COLUMN_CONFIRMED_BALANCE:
 			if (this.asset == null) return "-";
 			balance = account.getBalance(this.asset.getKey(DCSet.getInstance()));
-			str = NumberAsString.getInstance().numberAsString(balance.a) + "/" + balance.b.b.toPlainString() + "/" + balance.c.b.toPlainString();
+			str = NumberAsString.getInstance().numberAsString(balance.a.b) + "/" + balance.b.b.toPlainString() + "/" + balance.c.b.toPlainString();
 			return str;
 	/*
 		case COLUMN_WAINTING_BALANCE:
