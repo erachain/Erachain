c4e4c2627a9f34d3078e1a68a408fd657194e9f7
adm <adm@icreator>
2018-04-18 13:43:52 +0300

    ff

--- ERA/src/api/NamePaymentResource.java
+++ ERA/src/api/NamePaymentResource.java
@@ -22,37 +22,37 @@ import utils.Pair;
 @Produces(MediaType.APPLICATION_JSON)
 public class NamePaymentResource {
 
-	
+
 	private static final Logger LOGGER = Logger
 			.getLogger(NamePaymentResource.class);
-	
+
 	@Context
 	HttpServletRequest request;
-	
+
 	@POST
 	@Consumes(MediaType.WILDCARD)
 	public String namePayment(String x)
 	{
 		try
-		{				
+		{
 			//READ JSON
 			JSONObject jsonObject = (JSONObject) JSONValue.parse(x);
 			String assetKey = (String) jsonObject.get("assetKey");
 			String amount = (String) jsonObject.get("amount");
 			String feePow = (String) jsonObject.get("feePow");
 			String sender = (String) jsonObject.get("sender");
-			String nameName = (String) jsonObject.get("recipient");	
+			String nameName = (String) jsonObject.get("recipient");
 			String password = (String) jsonObject.get("password");
-			
+
 			Pair<Account, NameResult> nameToAdress = NameUtils.nameToAdress(nameName);
 			if(nameToAdress.getB() == NameResult.OK)
 			{
 				String recipient = nameToAdress.getA().getAddress();
 				password = null;
-				return APIUtils.processPayment(password, sender, feePow, recipient, assetKey,  amount, x, request);
+				return APIUtils.processPayment(password, sender, feePow, recipient, assetKey,  amount, x, request, jsonObject);
 			}else
 			{
-				return APIUtils.processPayment(password, sender, feePow, nameName, assetKey,  amount, x, request);
+				return APIUtils.processPayment(password, sender, feePow, nameName, assetKey,  amount, x, request, jsonObject);
 			}
 		}
 		catch(NullPointerException | ClassCastException e)
@@ -61,9 +61,9 @@ public class NamePaymentResource {
 			LOGGER.info(e);
 			throw ApiErrorFactory.getInstance().createError(ApiErrorFactory.ERROR_JSON);
 		}
-			
-			
+
+
 	}
-	
-	
+
+
 }
