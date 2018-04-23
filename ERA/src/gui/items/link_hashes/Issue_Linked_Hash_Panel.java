7a112f94a8311246ef7ed238aefa63b070c86b87
Ruslan <r.chornomaz@mail.ru>
2018-04-23 16:03:31 +0300

    fix button

--- ERA/src/gui/items/link_hashes/Issue_Linked_Hash_Panel.java
+++ ERA/src/gui/items/link_hashes/Issue_Linked_Hash_Panel.java
@@ -3,7 +3,7 @@ package gui.items.link_hashes;
 import core.crypto.Base58;
 import core.crypto.Crypto;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
+//import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -31,6 +31,12 @@ import gui.Split_Panel;
 import gui.library.My_JFileChooser;
 import lang.Lang;
 import utils.Pair;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Dimension;
+import javax.swing.SwingConstants;
 
 public class Issue_Linked_Hash_Panel extends Split_Panel {
 	Table_Model_Issue_Hashes table_Model;
@@ -40,6 +46,14 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 
 	public Issue_Linked_Hash_Panel() {
 		super("Issue_Linked_Hash_Panel");
+		jButton1_jToolBar_RightPanel.setVisible(false);
+		jButton2_jToolBar_RightPanel.setVisible(false);
+		GridBagLayout gridBagLayout = (GridBagLayout) rightPanel1.getLayout();
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0 };
+		gridBagLayout.columnWeights = new double[] { 0.0 };
+		// GridBagLayout gridBagLayout = (GridBagLayout) rightPanel1.getLayout();
+		// gridBagLayout.rowWeights = new double[]{1.0, 0.0};
+		// gridBagLayout.columnWeights = new double[]{1.0};
 		// left panel
 
 		this.toolBar_LeftPanel.setVisible(false);
@@ -54,43 +68,39 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 			}
 		});
 
-		// Right panel
-		// this.jToolBar_RightPanel.setSize(WIDTH, 200);
-		jButton2_jToolBar_RightPanel.setText(Lang.getInstance().translate("Import Hashs"));
-		// jButton2_jToolBar_RightPanel.setSize(70, 30);
-		// jButton2_jToolBar_RightPanel.setPreferredSize(new
-		// Dimension(100,200));
-		jButton2_jToolBar_RightPanel.addActionListener(new ActionListener() {
-			// create Hashs
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Hashs_from_Files(true);
+		JPanel panel = new JPanel();
+		jToolBar_RightPanel.add(panel);
 
-			}
-		});
-
-		jButton1_jToolBar_RightPanel.setText(Lang.getInstance().translate("Delete Hash"));
-		jButton1_jToolBar_RightPanel.addActionListener(new ActionListener() {
-			// delete row
-			@Override
+		JButton btnNewButton = new JButton();
+		btnNewButton.setText(Lang.getInstance().translate("Delete Hash"));
+		panel.add(btnNewButton);
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
 				int a = table_Model.getRowCount();
 				if (table_Model.getRowCount() > 1) {
 					int selRow = Table_Hash.getSelectedRow();
 					Object f = table_Model.getValueAt(selRow, 0);
-					if (selRow != -1 && table_Model.getRowCount()>=selRow) {
+					if (selRow != -1 && table_Model.getRowCount() >= selRow) {
 						((DefaultTableModel) table_Model).removeRow(selRow);
 						table_Model.fireTableDataChanged();
 					}
 				}
+
 			}
 		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.setVerticalTextPosition(SwingConstants.BOTTOM);
+		btnNewButton.setMinimumSize(new Dimension(33, 9));
+		btnNewButton.setMaximumSize(new Dimension(33, 9));
 
 		jButton3_jToolBar_RightPanel = new JButton();
+		panel.add(jButton3_jToolBar_RightPanel);
 		jButton3_jToolBar_RightPanel.setText(Lang.getInstance().translate("Create Hash"));
 		// jButton3_jToolBar_RightPanel.setFocusable(false);
-		jButton3_jToolBar_RightPanel.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
-	//	jButton3_jToolBar_RightPanel.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+		jButton3_jToolBar_RightPanel.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+		jButton3_jToolBar_RightPanel.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 		jButton3_jToolBar_RightPanel.addActionListener(new ActionListener() {
 			// create Hashs
 			@Override
@@ -99,15 +109,28 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 
 			}
 		});
-		GridBagConstraints gridConstr = new GridBagConstraints();
-		gridConstr.gridx = 2;
-		gridConstr.gridy = 0;
-		gridConstr.insets = new java.awt.Insets(1, 8, 8, 8);
-		rightPanel1.add(jButton3_jToolBar_RightPanel,gridConstr);
-
-		jButton1_jToolBar_RightPanel.setFont(new Font("Tahoma", 0, 14));
-		jButton2_jToolBar_RightPanel.setFont(new Font("Tahoma", 0, 14));
 		jButton3_jToolBar_RightPanel.setFont(new Font("Tahoma", 0, 14));
+
+		JButton btnNewButton_1 = new JButton();
+		btnNewButton_1.setText(Lang.getInstance().translate("Import Hashs"));
+		panel.add(btnNewButton_1);
+		btnNewButton_1.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Hashs_from_Files(true);
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setMaximumSize(new Dimension(32767, 32));
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_1.anchor = GridBagConstraints.NORTH;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		rightPanel1.add(panel_1, gbc_panel_1);
 
 		table_Model = new Table_Model_Issue_Hashes(0);
 		Table_Hash = new JTable(table_Model);
@@ -183,7 +206,7 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 		List<String> twins = R_Hashes.findTwins(DCSet.getInstance(), hashes);
 		if (!twins.isEmpty()) {
 			JOptionPane.showMessageDialog(new JFrame(),
-					Lang.getInstance().translate("Twin hashes")+": " + twins.toString(),
+					Lang.getInstance().translate("Twin hashes") + ": " + twins.toString(),
 					Lang.getInstance().translate("Error"), JOptionPane.ERROR_MESSAGE);
 			issue_Hash_Imprint.jButton.setEnabled(true);
 			return;
@@ -215,12 +238,11 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 		// false - если создаем хэш для файлов
 
 		// открыть диалог для файла
-		//JFileChooser chooser = new JFileChooser();
+		// JFileChooser chooser = new JFileChooser();
 		// руссификация диалога выбора файла
-		//new All_Options().setUpdateUI(chooser);
+		// new All_Options().setUpdateUI(chooser);
 		My_JFileChooser chooser = new My_JFileChooser();
 		chooser.setDialogTitle(Lang.getInstance().translate("Select File"));
-		
 
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		chooser.setMultiSelectionEnabled(true);
@@ -233,18 +255,16 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 
 		int returnVal = chooser.showOpenDialog(getParent());
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-// если есть пустые строки удаляем их
+			// если есть пустые строки удаляем их
 			int i;
-			for( i=0; i <= table_Model.getRowCount()-1; i++){
-				if (table_Model.getValueAt(i, 0).toString().equals("")){
-					
+			for (i = 0; i <= table_Model.getRowCount() - 1; i++) {
+				if (table_Model.getValueAt(i, 0).toString().equals("")) {
+
 					table_Model.removeRow(i);
-					
+
 				}
 			}
-			
-			
-			
+
 			if (importing) {
 				// IMPORT FROM FILE
 				File patch = chooser.getSelectedFile();
@@ -254,16 +274,18 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 					hashesStr = new String(Files.readAllBytes(Paths.get(file_name)));
 				} catch (IOException e) {
 					e.printStackTrace();
-					table_Model.addRow(new Object[] { "",
-							Lang.getInstance().translate("error reading") + " - " + file_name });
+					table_Model.addRow(
+							new Object[] { "", Lang.getInstance().translate("error reading") + " - " + file_name });
 				}
-				
+
 				if (hashesStr.length() > 0) {
-				String[] hashes = hashesStr.split("\\s*(\\s|,|!|;|:|\n|\\.)\\s*");
-					for (String hashB58: hashes) {
-						if (hashB58!= null && !hashB58.equals(new String("")))	table_Model.addRow(new Object[] { hashB58, Lang.getInstance().translate("imported from") + " " +  file_name});					
+					String[] hashes = hashesStr.split("\\s*(\\s|,|!|;|:|\n|\\.)\\s*");
+					for (String hashB58 : hashes) {
+						if (hashB58 != null && !hashB58.equals(new String("")))
+							table_Model.addRow(new Object[] { hashB58,
+									Lang.getInstance().translate("imported from") + " " + file_name });
 					}
-					
+
 				}
 
 			} else {
@@ -299,8 +321,8 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-						table_Model.addRow(new Object[] { "",
-								Lang.getInstance().translate("error reading") + " - " + file_name });
+						table_Model.addRow(
+								new Object[] { "", Lang.getInstance().translate("error reading") + " - " + file_name });
 						continue;
 					}
 					try {
@@ -313,19 +335,15 @@ public class Issue_Linked_Hash_Panel extends Split_Panel {
 
 					/// HASHING
 					String hashes = Base58.encode(Crypto.getInstance().digest(fileInArray));
-					table_Model.addRow(new Object[] { hashes,
-							Lang.getInstance().translate("from file ") + file_name });
+					table_Model.addRow(new Object[] { hashes, Lang.getInstance().translate("from file ") + file_name });
 
 				}
-				
-				
+
 			}
-			table_Model.addRow(new Object[] { "",""});
+			table_Model.addRow(new Object[] { "", "" });
 			table_Model.fireTableDataChanged();
-			Table_Hash.setRowSelectionInterval(table_Model.getRowCount()-1,table_Model.getRowCount()-1);
-			
-			
-			
+			Table_Hash.setRowSelectionInterval(table_Model.getRowCount() - 1, table_Model.getRowCount() - 1);
+
 		}
 
 	}
