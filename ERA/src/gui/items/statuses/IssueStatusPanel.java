3225a06fc65e51db6dcefd148110a5560d52bc0b
Ruslan <r.chornomaz@mail.ru>
2018-06-22 12:25:33 +0300

    change gui vertical align top

--- ERA/src/gui/items/statuses/IssueStatusPanel.java
+++ ERA/src/gui/items/statuses/IssueStatusPanel.java
@@ -1,5 +1,24 @@
 package gui.items.statuses;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
 import controller.Controller;
 import core.account.Account;
 import core.account.PrivateKeyAccount;
@@ -14,11 +33,6 @@ import gui.library.library;
 import gui.models.AccountsComboBoxModel;
 import gui.transaction.OnDealClick;
 import lang.Lang;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
 public class IssueStatusPanel extends JPanel {
@@ -35,18 +49,17 @@ public class IssueStatusPanel extends JPanel {
     // @SuppressWarnings({ "unchecked", "rawtypes" })
     public IssueStatusPanel() {
 
-        String colorText = "ff0000"; // цвет текста в форме
+        String colorText = "ff0000";
         th = this;
-        // CLOSE
-        // setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         // LAYOUT
         GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{33, 0, 0, 0, 0, 0, 0, 0, 65, 0, 0};
-        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-        gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0};
-        gridBagLayout.rowHeights = new int[]{-67, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        gridBagLayout.rowHeights = new int[] { 0, 0, 0, 83, 0, 0, 0, 0 };
+        gridBagLayout.columnWidths = new int[] { -16, 0, 0, 0, 216, 0, 0, 0 };
+        gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+        gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
         this.setLayout(gridBagLayout);
+
         // COMBOBOX GBC
         GridBagConstraints cbxGBC = new GridBagConstraints();
         // cbxGBC.insets = new Insets(5,5,5,5);
@@ -56,172 +69,151 @@ public class IssueStatusPanel extends JPanel {
         cbxGBC.gridx = 1;
         cbxGBC.insets = new java.awt.Insets(5, 3, 5, 15);
 
+        int gridy = 0;
+
+        // JLabel info_Label = new JLabel();
+        // info_Label.setText(Lang.getInstance().translate("Issue Status"));
+        // labelGBC.gridy = gridy;
+
         JLabel jLabel1 = new JLabel();
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText(Lang.getInstance().translate("Create Status"));
         jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         GridBagConstraints gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 9;
-        gridBagConstraints.anchor = GridBagConstraints.NORTH;
+        gridBagConstraints.gridwidth = 6;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
         gridBagConstraints.weightx = 1.3;
         gridBagConstraints.insets = new java.awt.Insets(8, 15, 8, 15);
         add(jLabel1, gridBagConstraints);
-        // LABEL FROM
-        JLabel fromLabel = new JLabel(Lang.getInstance().translate("Account") + ":");
-        GridBagConstraints gbc_fromLabel = new GridBagConstraints();
-        gbc_fromLabel.fill = GridBagConstraints.HORIZONTAL;
-        gbc_fromLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_fromLabel.gridx = 1;
-        gbc_fromLabel.gridy = 1;
-        this.add(fromLabel, gbc_fromLabel);
 
-        // COMBOBOX FROM
-        this.cbxFrom = new JComboBox<Account>(new AccountsComboBoxModel());
-        GridBagConstraints gbc_cbxFrom = new GridBagConstraints();
-        gbc_cbxFrom.fill = GridBagConstraints.HORIZONTAL;
-        gbc_cbxFrom.gridwidth = 8;
-        gbc_cbxFrom.insets = new Insets(0, 0, 5, 5);
-        gbc_cbxFrom.gridx = 2;
-        gbc_cbxFrom.gridy = 1;
-        this.add(this.cbxFrom, gbc_cbxFrom);
+        // this.add(info_Label, labelGBC);
+        gridy++;
 
-        // size from width
         add_Image_Panel = new My_Add_Image_Panel(
                 Lang.getInstance().translate("Add Image") + (" (max %1%kB)").replace("%1%", "1024"), 250, 250);
-
         GridBagConstraints gbc_add_Image_Panel = new GridBagConstraints();
         gbc_add_Image_Panel.anchor = GridBagConstraints.NORTH;
         gbc_add_Image_Panel.fill = GridBagConstraints.HORIZONTAL;
-        gbc_add_Image_Panel.gridheight = 8;
+        gbc_add_Image_Panel.gridheight = 3;
+        gbc_add_Image_Panel.gridwidth = 2;
         gbc_add_Image_Panel.insets = new Insets(0, 0, 5, 5);
         gbc_add_Image_Panel.gridx = 0;
         gbc_add_Image_Panel.gridy = 1;
         add(add_Image_Panel, gbc_add_Image_Panel);
+        JLabel fromLabel = new JLabel(Lang.getInstance().translate("Account") + ":");
+        GridBagConstraints gbc_fromLabel = new GridBagConstraints();
+        gbc_fromLabel.anchor = GridBagConstraints.WEST;
+        gbc_fromLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_fromLabel.gridx = 2;
+        gbc_fromLabel.gridy = 1;
 
-        // LABEL NAME
+        this.add(fromLabel, gbc_fromLabel);
+        this.cbxFrom = new JComboBox<Account>(new AccountsComboBoxModel());
+        GridBagConstraints gbc_cbxFrom = new GridBagConstraints();
+        gbc_cbxFrom.gridwidth = 3;
+        gbc_cbxFrom.fill = GridBagConstraints.HORIZONTAL;
+        gbc_cbxFrom.insets = new Insets(0, 0, 5, 5);
+        gbc_cbxFrom.gridx = 4;
+        gbc_cbxFrom.gridy = 1;
+        this.add(this.cbxFrom, gbc_cbxFrom);
         JLabel nameLabel = new JLabel(
                 "<HTML><p style=':#" + colorText + "'>" + Lang.getInstance().translate("Name") + ": </p></html>");
         GridBagConstraints gbc_nameLabel = new GridBagConstraints();
-        gbc_nameLabel.fill = GridBagConstraints.HORIZONTAL;
+        gbc_nameLabel.anchor = GridBagConstraints.WEST;
         gbc_nameLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_nameLabel.gridx = 1;
+        gbc_nameLabel.gridx = 2;
         gbc_nameLabel.gridy = 2;
         this.add(nameLabel, gbc_nameLabel);
+        this.txtName = new JTextField();
+        GridBagConstraints gbc_txtName = new GridBagConstraints();
+        gbc_txtName.gridwidth = 2;
+        gbc_txtName.fill = GridBagConstraints.BOTH;
+        gbc_txtName.insets = new Insets(0, 0, 5, 5);
+        gbc_txtName.gridx = 4;
+        gbc_txtName.gridy = 2;
+        this.add(txtName, gbc_txtName);
 
-        // TXT NAME
-        /*
-         * this.txtName = new JTextField(); GridBagConstraints gbc_txtName = new
-         * GridBagConstraints(); gbc_txtName.fill = GridBagConstraints.HORIZONTAL;
-         * gbc_txtName.gridwidth = 5; gbc_txtName.insets = new Insets(0, 0, 5, 5);
-         * gbc_txtName.gridx = 2; gbc_txtName.gridy = 2; this.add(this.txtName,
-         * gbc_txtName); this.txtareaDescription.setBorder(this.txtName.getBorder());
-         */
-        // size from height
         add_Logo_Icon_Panel = new My_Add_Image_Panel(Lang.getInstance().translate("Add Logo"), 50, 50);
+
         GridBagConstraints gbc_add_Logo_Icon_Panel = new GridBagConstraints();
-        gbc_add_Logo_Icon_Panel.gridwidth = 2;
-        gbc_add_Logo_Icon_Panel.gridheight = 2;
+        gbc_add_Logo_Icon_Panel.anchor = GridBagConstraints.EAST;
         gbc_add_Logo_Icon_Panel.insets = new Insets(0, 0, 5, 5);
-        gbc_add_Logo_Icon_Panel.gridx = 8;
+        gbc_add_Logo_Icon_Panel.gridx = 6;
         gbc_add_Logo_Icon_Panel.gridy = 2;
         add(add_Logo_Icon_Panel, gbc_add_Logo_Icon_Panel);
-
-        JLabel label_bottom = new JLabel();
-
-        GridBagConstraints gbc_label_bottom = new GridBagConstraints();
-        gbc_label_bottom.insets = new Insets(0, 0, 5, 0);
-        gbc_label_bottom.gridx = 10;
-        gbc_label_bottom.gridy = 2;
-        this.add(label_bottom, gbc_label_bottom);
-
-        this.setMinimumSize(new Dimension(0, 0));
-
-        // LABEL DESCRIPTION
         JLabel descriptionLabel = new JLabel(Lang.getInstance().translate("Description") + ":");
         GridBagConstraints gbc_descriptionLabel = new GridBagConstraints();
-        gbc_descriptionLabel.fill = GridBagConstraints.HORIZONTAL;
+        gbc_descriptionLabel.anchor = GridBagConstraints.NORTHWEST;
         gbc_descriptionLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_descriptionLabel.gridx = 1;
-        gbc_descriptionLabel.gridy = 4;
+        gbc_descriptionLabel.gridx = 2;
+        gbc_descriptionLabel.gridy = 3;
         this.add(descriptionLabel, gbc_descriptionLabel);
+
+        // TEXTFIELD GBC
 
         JScrollPane scrollDescription = new JScrollPane();
         scrollDescription.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollDescription.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         GridBagConstraints gbc_scrollDescription = new GridBagConstraints();
-        gbc_scrollDescription.fill = GridBagConstraints.HORIZONTAL;
-        gbc_scrollDescription.gridwidth = 8;
+        gbc_scrollDescription.gridwidth = 3;
+        gbc_scrollDescription.fill = GridBagConstraints.BOTH;
         gbc_scrollDescription.insets = new Insets(0, 0, 5, 5);
-        gbc_scrollDescription.gridx = 2;
-        gbc_scrollDescription.gridy = 4;
-        this.add(scrollDescription, gbc_scrollDescription);
+        gbc_scrollDescription.gridx = 4;
+        gbc_scrollDescription.gridy = 3;
 
-        // TXTAREA DESCRIPTION
+        this.add(scrollDescription, gbc_scrollDescription);
         this.txtareaDescription = new JTextArea();
         scrollDescription.setViewportView(txtareaDescription);
 
-        this.txtareaDescription.setRows(6);
-        this.txtareaDescription.setColumns(20);
-
-        // LABEL FEE POW
         JLabel feeLabel = new JLabel(Lang.getInstance().translate("Fee Power") + " (0..6)" + ":");
         GridBagConstraints gbc_feeLabel = new GridBagConstraints();
         gbc_feeLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_feeLabel.gridx = 1;
-        gbc_feeLabel.gridy = 5;
+        gbc_feeLabel.gridx = 2;
+        gbc_feeLabel.gridy = 4;
         this.add(feeLabel, gbc_feeLabel);
-
-        // TXT FEE POW
         this.txtFeePow = new JTextField();
         this.txtFeePow.setText("0");
         GridBagConstraints gbc_txtFeePow = new GridBagConstraints();
+        gbc_txtFeePow.gridwidth = 3;
         gbc_txtFeePow.fill = GridBagConstraints.HORIZONTAL;
-        gbc_txtFeePow.gridwidth = 8;
         gbc_txtFeePow.insets = new Insets(0, 0, 5, 5);
-        gbc_txtFeePow.gridx = 2;
-        gbc_txtFeePow.gridy = 5;
+        gbc_txtFeePow.gridx = 4;
+        gbc_txtFeePow.gridy = 4;
         this.add(this.txtFeePow, gbc_txtFeePow);
 
-        // JCheckBox jCheck_Unique;
+        // PACK
+        // this.pack();
+        // this.setResizable(false);
+        // this.setLocationRelativeTo(null);
+        this.setMinimumSize(new Dimension(0, 0));
         JLabel unoqueLabel = new JLabel(Lang.getInstance().translate("Single") + ":");
         GridBagConstraints gbc_unoqueLabel = new GridBagConstraints();
         gbc_unoqueLabel.insets = new Insets(0, 0, 5, 5);
-        gbc_unoqueLabel.gridx = 1;
-        gbc_unoqueLabel.gridy = 6;
+        gbc_unoqueLabel.gridx = 2;
+        gbc_unoqueLabel.gridy = 5;
         this.add(unoqueLabel, gbc_unoqueLabel);
-
         jCheck_Unique = new JCheckBox();
         GridBagConstraints gbc_jCheck_Unique = new GridBagConstraints();
+        gbc_jCheck_Unique.anchor = GridBagConstraints.WEST;
         gbc_jCheck_Unique.insets = new Insets(0, 0, 5, 5);
-        gbc_jCheck_Unique.gridx = 2;
-        gbc_jCheck_Unique.gridy = 6;
+        gbc_jCheck_Unique.gridx = 4;
+        gbc_jCheck_Unique.gridy = 5;
         this.add(this.jCheck_Unique, gbc_jCheck_Unique);
 
-        // BUTTON GBC
-        GridBagConstraints buttonGBC = new GridBagConstraints();
-        buttonGBC.anchor = GridBagConstraints.NORTHEAST;
-        buttonGBC.insets = new Insets(8, 5, 5, 15);
-        buttonGBC.gridx = 0;
-
-        // BUTTON Register
-        buttonGBC.gridy = 7;
-        buttonGBC.gridx = 1;
-
-        this.issueButton = new JButton("Issue");
+        this.issueButton = new JButton(Lang.getInstance().translate("Issue"));
         this.issueButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onIssueClick();
             }
         });
         GridBagConstraints gbc_issueButton = new GridBagConstraints();
-        gbc_issueButton.gridwidth = 3;
         gbc_issueButton.insets = new Insets(0, 0, 5, 5);
-        gbc_issueButton.gridx = 7;
-        gbc_issueButton.gridy = 7;
+        gbc_issueButton.gridx = 6;
+        gbc_issueButton.gridy = 6;
         this.add(this.issueButton, gbc_issueButton);
         this.setVisible(true);
     }
@@ -259,9 +251,9 @@ public class IssueStatusPanel extends JPanel {
         } catch (Exception e) {
             String mess = "Invalid pars... " + parse;
             switch (parse) {
-                case 0:
-                    mess = "Invalid fee power 0..6";
-                    break;
+            case 0:
+                mess = "Invalid fee power 0..6";
+                break;
             }
             JOptionPane.showMessageDialog(new JFrame(), Lang.getInstance().translate(e + mess),
                     Lang.getInstance().translate("Error"), JOptionPane.ERROR_MESSAGE);
@@ -302,7 +294,7 @@ public class IssueStatusPanel extends JPanel {
         dd.setVisible(true);
 
         // JOptionPane.OK_OPTION
-        if (!dd.isConfirm) {
+        if (!dd.isConfirm) { // s!= JOptionPane.OK_OPTION) {
 
             this.issueButton.setEnabled(true);
 
