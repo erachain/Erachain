f7806a3b9301c9ad38fd9c7822696268dc6918e7
icreator <icreator@mail.ru>
2018-07-31 11:05:30 +0300

    private Paair -> privateKey class
    вернул обратно в приватный клас приватную переменную

--- ERA/src/core/account/PrivateKeyAccount.java
+++ ERA/src/core/account/PrivateKeyAccount.java
@@ -9,15 +9,12 @@ import java.util.Arrays;
 public class PrivateKeyAccount extends PublicKeyAccount {
 
     private byte[] seed;
+    private Pair<byte[], byte[]> keyPair;
 
     public PrivateKeyAccount(byte[] seed) {
-        super(Crypto.getInstance().createKeyPair(seed));
+        super(Crypto.getInstance().createKeyPair(seed).getB());
         this.seed = seed;
-        //this.keyPair = ;
-        //this.publicKey = keyPair.getB();
-        //this.address = Crypto.getInstance().getAddress(this.publicKey);
-        //this.bytes = Base58.decode(address);
-        //this.shortBytes = Arrays.copyOfRange(this.bytes, 5, this.bytes.length);
+        this.keyPair = Crypto.getInstance().createKeyPair(seed);
     }
 
     public byte[] getSeed() {
