9af263141590ec0a0ff6e1754b014d9f4767b9e4
Саша <rty@yandex.ru>
2018-09-29 11:47:37 +1000

    GUI Telegram
    telegram spli panel +

--- ERA/src/gui/telegrams/TelegramSplitPanel.java
+++ ERA/src/gui/telegrams/TelegramSplitPanel.java
@@ -185,7 +185,10 @@ public class TelegramSplitPanel extends Split_Panel {
         @Override
         public void valueChanged(ListSelectionEvent e) {
             // TODO Auto-generated method stub
-           if( tableFavoriteAccounts.getSelectedRow() <= 0) return;;
+           if( tableFavoriteAccounts.getSelectedRow() < 0) return;;
+           int a1 = tableFavoriteAccounts.getSelectedRow();
+           if(a1 >= accountModel.getRowCount()) return;
+           int b1 = tableFavoriteAccounts.convertRowIndexToModel(tableFavoriteAccounts.getSelectedRow());
             String account = (String) accountModel.getValueAt((tableFavoriteAccounts.convertRowIndexToModel(tableFavoriteAccounts.getSelectedRow())),accountModel.COLUMN_ADDRESS);
             rightTelegramPanel.jLabelRaght.setText(account);
             rightTelegramPanel.walletTelegramsFilterTableModel.setReciever(account);
@@ -211,7 +214,7 @@ public class TelegramSplitPanel extends Split_Panel {
        else{
            this.leftTelegram.jCxbRecipientmessages.setSelected(true);
            tableFavoriteAccounts.setVisible(true);  
-           if( tableFavoriteAccounts.getSelectedRow() <= 0) return;;
+           if( tableFavoriteAccounts.getSelectedRow() < 0) return;;
            String account = (String) accountModel.getValueAt((tableFavoriteAccounts.convertRowIndexToModel(tableFavoriteAccounts.getSelectedRow())),accountModel.COLUMN_ADDRESS);
            rightTelegramPanel.jLabelRaght.setText(account);
            rightTelegramPanel.walletTelegramsFilterTableModel.setReciever(account);
