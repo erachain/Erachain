9d3c80a4246be7eb4fabc587ce010806f6f132c3
icreator <icreator@mail.ru>
2018-10-22 13:18:51 +0300

    fix getJobLevel for letters
    снизил до 150 минимальное

--- ERA/src/core/transaction/TransactionAmount.java
+++ ERA/src/core/transaction/TransactionAmount.java
@@ -1141,6 +1141,9 @@ public abstract class TransactionAmount extends Transaction {
 
     @Override
     public int getJobLevel() {
+        if (this.amount == null || this.amount.signum() == 0) {
+            return 150;
+        }
         return 300;
     }
 
