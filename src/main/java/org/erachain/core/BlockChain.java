5948d850d1ad6575a421ca6eece501eb3b6f062f
icreator <icreator@mail.ru>
2018-11-09 12:48:40 +0300

    поправил ITEM_POLL_FROM до запуска 4.11

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -148,7 +148,7 @@ public class BlockChain {
                     //Base58.decode("61Fzu3PhsQ74EoMKrwwxKHMQi3z9fYAU5UeUfxtGdXPRfKbWdgpBQWgAojEnmDHK2LWUKtsmyqWb4WpCEatthdgK"),
             };
 
-    public static final int ITEM_POLL_FROM = DEVELOP_USE ? 77000 : 130033;
+    public static final int ITEM_POLL_FROM = DEVELOP_USE ? 77000 : VERS_4_11;
 
     public static final int AMOUNT_SCALE_FROM = DEVELOP_USE ? 1034 : 1033;
     public static final int AMOUNT_DEDAULT_SCALE = 8;
