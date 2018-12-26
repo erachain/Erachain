562d17efce7e078b403ab9291e32db787d7c7968
icreator <icreator@mail.ru>
2018-12-26 11:46:31 +0300

    fix: при старте путь кошелька из параметтров

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -641,13 +641,13 @@ public class Controller extends Observable {
                 if (accsNum > 0) {
 
                     String path;
-                    if (Start.seedCommand.length == 4) {
+                    if (Start.seedCommand.length > 3) {
                         path = Start.seedCommand[3];
                     } else {
                         path = Settings.getInstance().getWalletDir();
                     }
 
-                    boolean res = recoverWallet(seed, Start.seedCommand[2], accsNum, path);
+                    boolean res = recoverWallet(seed, path, accsNum, path);
                     Start.seedCommand = null;
                 }
             }
