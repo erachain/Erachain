94a479e1fe987003d8b323c8fcde29aa8cd0aa81
Саша <rty@yandex.ru>
2019-01-15 21:44:27 +1000

    корректное закрытие главаного окна

--- src/main/java/org/erachain/Start.java
+++ src/main/java/org/erachain/Start.java
@@ -1,30 +1,10 @@
 package org.erachain;
 
-import org.erachain.api.ApiClient;
 import org.erachain.controller.Controller;
-import org.erachain.core.BlockChain;
-import org.erachain.gui.AboutFrame;
-import org.erachain.gui.Gui;
-import org.erachain.gui.library.Issue_Confirm_Dialog;
-import org.erachain.lang.Lang;
-import org.erachain.settings.Settings;
-import org.erachain.utils.SysTray;
-import org.erachain.webserver.Status;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
-import javax.swing.*;
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.Scanner;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+//@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class Start {
 
     //@Bean(name = DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
