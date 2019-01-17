85ad28cce0848fb7a5b0e270222824a96868708d
icreator <icreator@mail.ru>
2019-01-17 12:14:11 +0300

    убрал ожидание останова Слушателя Сети

--- src/main/java/org/erachain/network/ConnectionAcceptor.java
+++ src/main/java/org/erachain/network/ConnectionAcceptor.java
@@ -2,9 +2,9 @@ package org.erachain.network;
 
 import org.erachain.controller.Controller;
 import org.erachain.database.PeerMap;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import org.erachain.settings.Settings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -107,6 +107,8 @@ public class ConnectionAcceptor extends Thread {
     public void halt() {
         //this.interrupt();
         this.isRun = false;
+
+        LOGGER.info("on halt");
 
         try {
             socket.close();
