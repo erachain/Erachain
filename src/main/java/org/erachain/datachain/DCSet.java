7605723650d9b548f89a1ce9c62b03be9444c3c0
icreator <icreator@mail.ru>
2019-02-03 11:43:24 +0300

    compressionEnable

--- src/main/java/org/erachain/datachain/DCSet.java
+++ src/main/java/org/erachain/datachain/DCSet.java
@@ -384,8 +384,10 @@ public class DCSet implements Observer, IDB {
                 //.asyncWriteFlushDelay(100)
                 //.cacheHardRefEnable()
 
-                // если при записи на доск блока процессор сильно нагружается - то уменьшить это
-                .freeSpaceReclaimQ(0) // не нагружать процессор для поиска свободного места в базе данных
+                // если при записи на диск блока процессор сильно нагружается - то уменьшить это
+                .freeSpaceReclaimQ(3) // не нагружать процессор для поиска свободного места в базе данных
+
+                .compressionEnable()
 
                 /*
                 .cacheSize(CASH_SIZE)
