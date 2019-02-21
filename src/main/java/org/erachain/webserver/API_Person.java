760b278ca76562941dc0dd2580931966bb13da72
icreator <icreator@mail.ru>
2019-02-21 19:00:24 +0300

    API Person - status

--- src/main/java/org/erachain/webserver/API_Person.java
+++ src/main/java/org/erachain/webserver/API_Person.java
@@ -1,17 +1,13 @@
 package org.erachain.webserver;
 
-import org.erachain.api.ApiErrorFactory;
 import org.erachain.controller.Controller;
-import org.erachain.core.blockexplorer.BlockExplorer;
 import org.erachain.core.crypto.Base58;
-import org.erachain.core.item.assets.Order;
-import org.erachain.core.item.assets.Trade;
-import org.erachain.core.transaction.Transaction;
 import org.erachain.datachain.DCSet;
 import org.erachain.datachain.ItemAssetBalanceMap;
-import org.erachain.datachain.ItemAssetMap;
-import org.erachain.datachain.TransactionFinalMap;
+import org.erachain.datachain.KKPersonStatusMap;
 import org.erachain.utils.StrJSonFine;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.mapdb.Fun;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,10 +16,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Path("apiperson")
 @Produces(MediaType.APPLICATION_JSON)
@@ -39,8 +32,8 @@ public class API_Person {
     public Response Default() {
         Map<String, String> help = new LinkedHashMap<>();
 
-        help.put("apiperson/balance/{asset}?position={position}",
-                "Get balance in Position for Asset.");
+        help.put("apiperson/balance/{personKey}/{assetKey}/{position}",
+                "Get Asset Key balance in Position [1..5] for Person Key.");
 
         return Response.status(200).header("Content-Type", "application/json; charset=utf-8")
                 .header("Access-Control-Allow-Origin", "*").entity(StrJSonFine.convert(help)).build();
@@ -50,13 +43,13 @@ public class API_Person {
     /**
      * @param personKey
      * @param assetKey
-     * @param pos
+     * @param pos 1..5
      * @return
      */
     @GET
-    @Path("balance/{person}/{asset}")
-    public Response getTradeByAccount(@PathParam("person") long personKey, @PathParam("asset") long assetKey,
-                                      @QueryParam("position") int pos) {
+    @Path("balance/{person}/{asset}/{position}")
+    public Response getBalance(@PathParam("person") long personKey, @PathParam("asset") long assetKey,
+                               @PathParam("position") int pos) {
 
         Set<String> addresses = DCSet.getInstance().getPersonAddressMap().getItems(personKey).keySet();
 
@@ -64,35 +57,35 @@ public class API_Person {
         ItemAssetBalanceMap map = DCSet.getInstance().getAssetBalanceMap();
         BigDecimal sum = BigDecimal.ZERO;
         switch (pos) {
-            case 0:
+            case 1:
                 for (String address : addresses) {
                     Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
                             Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balances = map.get(address, assetKey);
                     sum = sum.add(balances.a.b);
                 }
                 break;
-            case 1:
+            case 2:
                 for (String address : addresses) {
                     Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
                             Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balances = map.get(address, assetKey);
                     sum = sum.add(balances.b.b);
                 }
                 break;
-            case 2:
+            case 3:
                 for (String address : addresses) {
                     Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
                             Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balances = map.get(address, assetKey);
                     sum = sum.add(balances.c.b);
                 }
                 break;
-            case 3:
+            case 4:
                 for (String address : addresses) {
                     Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
                             Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balances = map.get(address, assetKey);
                     sum = sum.add(balances.d.b);
                 }
                 break;
-            case 4:
+            case 5:
                 for (String address : addresses) {
                     Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
                             Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balances = map.get(address, assetKey);
@@ -111,229 +104,68 @@ public class API_Person {
                 .entity(sum.toPlainString()).build();
     }
 
-    /**
-     * Get orders. The number of items in SEL and BUY is limited by LIMIT
-     * param.
-     *
-     * @param have  is HaveKey
-     * @param want  is WantKey
-     * @param limit count out record, default = 20
-     * @return orders for SELL and BUY
-     * @author Icreator
-     */
-
-    @GET
-    @Path("orders")
-    // apitrade/orders?have=1&want=2&limit=20
-    public Response orders(@QueryParam("have") Long have, @QueryParam("want") Long want,
-                           @DefaultValue("20") @QueryParam("limit") Long limit) {
-
-        ItemAssetMap map = this.dcSet.getItemAssetMap();
-        // DOES ASSETID EXIST
-        if (!map.contains(have)) {
-            throw ApiErrorFactory.getInstance().createError(
-                    //ApiErrorFactory.ERROR_INVALID_ASSET_ID);
-                    Transaction.ITEM_ASSET_NOT_EXIST);
-        }
-        if (!map.contains(want)) {
-            throw ApiErrorFactory.getInstance().createError(
-                    //ApiErrorFactory.ERROR_INVALID_ASSET_ID);
-                    Transaction.ITEM_ASSET_NOT_EXIST);
-        }
-
-        Map output = new LinkedHashMap();
-
-        List<Order> ordersHave = dcSet.getOrderMap().getOrdersForTradeWithFork(have, want, false);
-        List<Order> ordersWant = dcSet.getOrderMap().getOrdersForTradeWithFork(want, have, true);
-
-        Map sellsJSON = new LinkedHashMap();
-        Map buysJSON = new LinkedHashMap();
-
-        BigDecimal sumAmount = BigDecimal.ZERO;
-
-        BigDecimal sumSellingAmount = BigDecimal.ZERO;
-
-        BigDecimal vol;
-        int counter = 0;
-        // show SELLs in BACK order
-        for (int i = ordersHave.size() - 1; i >= 0; i--) {
-
-            Order order = ordersHave.get(i);
-            Map sellJSON = new LinkedHashMap();
-
-            sellJSON.put("price", order.getPrice());
-            vol = order.getAmountHaveLeft();
-            sellJSON.put("amount", vol);
-            sumAmount = sumAmount.add(vol);
-
-            sellJSON.put("sellingPrice", Order.calcPrice(order.getAmountWant(), order.getAmountHave()));
-
-            BigDecimal sellingAmount = order.getAmountWantLeft();
-
-            sellJSON.put("sellingAmount", sellingAmount);
-
-            sumSellingAmount = sumSellingAmount.add(sellingAmount);
-
-            sellsJSON.put(order.getId().toString(), sellJSON);
-
-            if (counter++ > limit) break;
-
-        }
-
-        output.put("sells", sellsJSON);
-
-        output.put("sellsSumAmount", sumAmount);
-        output.put("sellsSumTotal", sumSellingAmount);
-
-        sumAmount = BigDecimal.ZERO;
-
-        BigDecimal sumBuyingAmount = BigDecimal.ZERO;
-
-        counter = 0;
-        for (int i = ordersWant.size() - 1; i >= 0; i--) {
-
-            Order order = ordersWant.get(i);
-
-            Map buyJSON = new LinkedHashMap();
-
-            buyJSON.put("price", order.getPrice());
-            vol = order.getAmountHaveLeft();
-            buyJSON.put("amount", vol);
-
-            sumAmount = sumAmount.add(vol);
-
-            buyJSON.put("buyingPrice", Order.calcPrice(order.getAmountWant(), order.getAmountHave()));
-
-            BigDecimal buyingAmount = order.getAmountWantLeft();
-
-            buyJSON.put("buyingAmount", buyingAmount);
-
-            sumBuyingAmount = sumBuyingAmount.add(buyingAmount);
-
-            buysJSON.put(order.getId(), buyJSON);
-            //buysJSON.put(Base58.encode(order.a.a, 64), buyJSON);
-
-            if (counter++ > limit) break;
-
-        }
-        output.put("buys", buysJSON);
-
-        output.put("buysSumAmount", sumBuyingAmount);
-        output.put("buysSumTotal", sumAmount);
-
-        return Response.status(200)
-                .header("Content-Type", "application/json; charset=utf-8")
-                .header("Access-Control-Allow-Origin", "*")
-                .entity(StrJSonFine.convert(output))
-                //.entity(output.toJSONString())
-                .build();
-
-    }
 
     /**
-     * Get trades. The number of items in SEL and BUY is limited by LIMIT
-     * param.
-     *
-     * @param have  is HaveKey
-     * @param want  is WantKey
-     * @param limit count out record, default = 100
-     * @return trades
-     * @author Icreator
+     * Get Status for Person
+     * @param personKey
+     * @param statusKey
+     * @param history true - get history of changes
+     * @return
      */
-
     @GET
-    @Path("trades")
-    // apitrade/trades?have=1&want=2&limit=20
-    public Response trades(@QueryParam("have") Long have, @QueryParam("want") Long want,
-                           @DefaultValue("100") @QueryParam("limit") Long limit) {
+    @Path("status/{person}/{status}")
+    // http://127.0.0.1:9067/apiperson/status/1/11?history=true
+    public Response getStatus(@PathParam("person") long personKey, @PathParam("status") long statusKey,
+                              @QueryParam("history") boolean history) {
 
-        ItemAssetMap map = this.dcSet.getItemAssetMap();
-        // DOES ASSETID EXIST
-        if (!map.contains(have)) {
-            throw ApiErrorFactory.getInstance().createError(
-                    //ApiErrorFactory.ERROR_INVALID_ASSET_ID);
-                    Transaction.ITEM_ASSET_NOT_EXIST);
-        }
-        if (!map.contains(want)) {
-            throw ApiErrorFactory.getInstance().createError(
-                    //ApiErrorFactory.ERROR_INVALID_ASSET_ID);
-                    Transaction.ITEM_ASSET_NOT_EXIST);
-        }
+        Set<String> addresses = DCSet.getInstance().getPersonAddressMap().getItems(personKey).keySet();
 
-        Map output = new LinkedHashMap();
+        Controller cont = Controller.getInstance();
+        KKPersonStatusMap map = DCSet.getInstance().getPersonStatusMap();
+        TreeMap<Long, Stack<Fun.Tuple5<Long, Long, byte[], Integer, Integer>>> statuses = map.get(personKey);
+        if (statuses == null)
+            return Response.status(200).header("Content-Type", "application/json; charset=utf-8")
+                    .header("Access-Control-Allow-Origin", "*")
+                    .entity("person not found").build();
 
-        Map tradesJSON = new LinkedHashMap();
+        Stack<Fun.Tuple5<Long, Long, byte[], Integer, Integer>> status = statuses.get(statusKey);
+        if (status == null)
+            return Response.status(200).header("Content-Type", "application/json; charset=utf-8")
+                    .header("Access-Control-Allow-Origin", "*")
+                    .entity("status not found").build();
 
-        List<Trade> trades = dcSet.getTradeMap().getTrades(have,
-                want);
 
-        output.put("tradesCount", trades.size());
+        Fun.Tuple5<Long, Long, byte[], Integer, Integer> last = status.peek();
 
-        BigDecimal tradeWantAmount = BigDecimal.ZERO;
-        BigDecimal tradeHaveAmount = BigDecimal.ZERO;
+        JSONArray lastJSON = new JSONArray();
+        lastJSON.add(last.a);
+        lastJSON.add(last.b);
+        lastJSON.add(Base58.encode(last.c));
+        lastJSON.add(last.d);
+        lastJSON.add(last.e);
 
-        TransactionFinalMap finalMap = DCSet.getInstance().getTransactionFinalMap();
-        Transaction createOrder;
+        JSONObject out = new JSONObject();
+        out.put("last", lastJSON);
 
-        if (limit > 200)
-            limit = 200l;
+        if (history) {
+            JSONArray historyJSON = new JSONArray();
+            Iterator<Fun.Tuple5<Long, Long, byte[], Integer, Integer>> iterator = status.iterator();
+            iterator.next();
 
-        int i = 0;
-        for (Trade trade : trades) {
-            if (i++ > limit) break;
-
-            Map tradeJSON = new LinkedHashMap();
-
-            Order orderInitiator = Order.getOrder(dcSet, trade.getInitiator());
-
-            Order orderTarget = Order.getOrder(dcSet, trade.getTarget());
-
-            tradeJSON.put("amountHave", trade.getAmountHave());
-            tradeJSON.put("amountWant", trade.getAmountWant());
-
-            tradeJSON.put("realPrice", trade.calcPrice());
-            tradeJSON.put("realReversePrice", trade.calcPriceRevers());
-
-            createOrder = finalMap.get(orderInitiator.getId());
-            tradeJSON.put("initiatorTxSignature", Base58.encode(createOrder.getSignature()));
-            tradeJSON.put("initiatorId", orderInitiator.getId());
-
-            tradeJSON.put("initiatorCreator", orderInitiator.getCreator().getAddress());
-            tradeJSON.put("initiatorAmount", orderInitiator.getAmountHave());
-            if (orderInitiator.getHave() == have) {
-                tradeJSON.put("type", "sell");
-                tradeWantAmount = tradeWantAmount.add(trade.getAmountHave());
-                tradeHaveAmount = tradeHaveAmount.add(trade.getAmountWant());
-
-            } else {
-                tradeJSON.put("type", "buy");
-                tradeHaveAmount = tradeHaveAmount.add(trade.getAmountHave());
-                tradeWantAmount = tradeWantAmount.add(trade.getAmountWant());
+            while (iterator.hasNext()) {
+                Fun.Tuple5<Long, Long, byte[], Integer, Integer> item = iterator.next();
+                historyJSON.add(item.a);
+                historyJSON.add(item.b);
+                historyJSON.add(Base58.encode(item.c));
+                historyJSON.add(item.d);
+                historyJSON.add(item.e);
             }
-
-            createOrder = finalMap.get(orderInitiator.getId());
-            tradeJSON.put("targetTxSignature", Base58.encode(createOrder.getSignature()));
-            tradeJSON.put("targetId", orderTarget.getId());
-            tradeJSON.put("targetCreator", orderTarget.getCreator().getAddress());
-            tradeJSON.put("targetAmount", orderTarget.getAmountHave());
-
-            tradeJSON.put("timestamp", trade.getTimestamp());
-            tradeJSON.put("dateTime", BlockExplorer.timestampToStr(trade.getTimestamp()));
-
-            tradesJSON.put(i, tradeJSON);
+            out.put("history", historyJSON);
         }
-        output.put("trades", tradesJSON);
-
-        output.put("tradeWantAmount", tradeWantAmount);
-        output.put("tradeHaveAmount", tradeHaveAmount);
-
-        return Response.status(200)
-                .header("Content-Type", "application/json; charset=utf-8")
+        return Response.status(200).header("Content-Type", "application/json; charset=utf-8")
                 .header("Access-Control-Allow-Origin", "*")
-                .entity(StrJSonFine.convert(output))
-                //.entity(output.toJSONString())
-                .build();
-
+                .entity(out.toJSONString()).build();
     }
+
 
 }
