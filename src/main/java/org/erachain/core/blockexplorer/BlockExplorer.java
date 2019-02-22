07e14b9a94ae697df3414e6ba06f08a3b3dc31b1
icreator <icreator@mail.ru>
2019-02-23 00:54:14 +0300

    person - asset balances

--- src/main/java/org/erachain/core/blockexplorer/BlockExplorer.java
+++ src/main/java/org/erachain/core/blockexplorer/BlockExplorer.java
@@ -1603,7 +1603,9 @@ public class BlockExplorer {
         output.put("asset_key", asset.getKey());
         output.put("asset_name", asset.getName());
 
+        output.put("Label_sum", Lang.getInstance().translate_from_langObj("SUM", langObj));
         BigDecimal sum = PersonCls.getBalance(personKey, assetKey, position);
+        output.put("sum", sum);
 
         return output;
     }
