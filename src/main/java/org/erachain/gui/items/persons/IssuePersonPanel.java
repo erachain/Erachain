31a60dba7849d3c5de6791693e1ae851c6b70734
Pavel S <p.sergeev@ch-group.ru>
2019-03-29 12:03:13 +0300

    Добавлены библиотеки

--- src/main/java/org/erachain/gui/items/persons/IssuePersonPanel.java
+++ src/main/java/org/erachain/gui/items/persons/IssuePersonPanel.java
@@ -27,10 +27,8 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.*;
 import java.util.Calendar;
 import java.util.TimeZone;
 
