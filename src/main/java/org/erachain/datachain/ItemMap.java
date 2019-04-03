86db46e80fcdf4bee9e4ee3974bece627e2d3196
icreator <icreator@mail.ru>
2019-04-03 19:09:36 +0300

    fix TableModelOwnerPersons

--- src/main/java/org/erachain/datachain/ItemMap.java
+++ src/main/java/org/erachain/datachain/ItemMap.java
@@ -1,14 +1,12 @@
 package org.erachain.datachain;
 
+import org.erachain.controller.Controller;
 import org.erachain.core.item.ItemCls;
 import org.erachain.database.DBMap;
-import org.mapdb.BTreeMap;
-import org.mapdb.Fun;
+import org.erachain.utils.ReverseComparator;
+import org.mapdb.*;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.mapdb.Atomic;
-import org.mapdb.DB;
-import org.erachain.utils.Pair;
 
 import java.util.*;
 
@@ -24,11 +22,21 @@ public abstract class ItemMap extends DCMap<Long, ItemCls> {
 
     protected Atomic.Long atomicKey;
     protected long key;
+    protected String name;
+
+    protected BTreeMap ownerKeyMap;
+
+    private static final int NAME_INDEX = 1;
+
+    private NavigableSet<Fun.Tuple2<String, Long>> nameIndex;
+    private NavigableSet<Fun.Tuple2<String, Long>> nameDescendingIndex;
+
 
     public ItemMap(DCSet databaseSet, DB database, String name) {
         super(databaseSet, database);
 
         atomicKey = database.getAtomicLong(name + "_key");
+        this.name = name;
         key = atomicKey.get();
     }
 
@@ -67,7 +75,52 @@ public abstract class ItemMap extends DCMap<Long, ItemCls> {
         this.key = key;
     }
 
+
+    protected void makeOwnerKey(DB database) {
+
+        //////////////// NOT PROTOCOL INDEXES
+
+        //CHECK IF NOT MEMORY DATABASE
+        if (parent != null) {
+            return;
+        }
+
+        //PAIR KEY
+        this.ownerKeyMap = database.createTreeMap(name + "owner_item_key")
+                //.comparator(Fun.TUPLE3_COMPARATOR)
+                .makeOrGet();
+
+        //BIND OWNER KEY
+        Bind.secondaryKey((BTreeMap)map, this.ownerKeyMap, new Fun.Function2<String, Long, ItemCls>() {
+            @Override
+            public String run(Long key, ItemCls value) {
+                return value.getOwner().getAddress();
+            }
+        });
+
+    }
+
+
+    @SuppressWarnings("unchecked")
     protected void createIndexes(DB database) {
+        if (Controller.getInstance().onlyProtocolIndexing){
+            // NOT USE SECONDARY INDEXES
+            return;
+        }
+
+        //NAME INDEX
+        nameIndex = database.createTreeSet("pp")
+                .comparator(Fun.COMPARATOR)
+                .makeOrGet();
+
+        nameDescendingIndex = database.createTreeSet("ppd")
+                .comparator(new ReverseComparator(Fun.COMPARATOR))
+                .makeOrGet();
+
+        createIndex(NAME_INDEX, nameIndex, nameDescendingIndex, (a, b) -> {
+            return b.getName();
+        });
+
     }
 
     @Override
@@ -195,5 +248,18 @@ public abstract class ItemMap extends DCMap<Long, ItemCls> {
     public Collection<Long> getFromToKeys(long fromKey, long toKey) {
         return ((BTreeMap)map).subMap(fromKey, toKey).values();
     }
+
+    public NavigableMap<Long, ItemCls> getOwnerItems(String ownerPublicKey) {
+        return this.ownerKeyMap.subMap(ownerPublicKey, ownerPublicKey);
+    }
+
+    public NavigableSet<Fun.Tuple2<String, Long>> getNameIndex() {
+        return nameIndex;
+    }
+
+    public NavigableSet<Fun.Tuple2<String, Long>> nameDescendingIndex() {
+        return nameDescendingIndex;
+    }
+
 
 }
