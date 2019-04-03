945b951ca1849b7574dcc282a544349656e4779b
icreator <icreator@mail.ru>
2019-04-03 15:57:41 +0300

    fix PersonStatusesModel

--- src/main/java/org/erachain/gui/models/PersonStatusesModel.java
+++ src/main/java/org/erachain/gui/models/PersonStatusesModel.java
@@ -6,6 +6,7 @@ import org.erachain.core.item.statuses.StatusCls;
 import org.erachain.core.transaction.Transaction;
 import org.erachain.datachain.DCSet;
 import org.erachain.datachain.ItemStatusMap;
+import org.erachain.datachain.KKPersonStatusMap;
 import org.erachain.lang.Lang;
 import org.mapdb.Fun.Tuple2;
 import org.mapdb.Fun.Tuple5;
@@ -19,7 +20,7 @@ import java.util.*;
 ////////
 
 @SuppressWarnings("serial")
-public class PersonStatusesModel extends AbstractTableModel implements Observer {
+public class PersonStatusesModel extends TimerTableModelCls<Tuple2<Long, Tuple5<Long, Long, byte[], Integer, Integer>>> {
 
     public static final int COLUMN_STATUS_NAME = 0;
     public static final int COLUMN_PERIOD = 1;
@@ -42,101 +43,37 @@ public class PersonStatusesModel extends AbstractTableModel implements Observer 
     ItemStatusMap statusesMap;
     long itemKey;
     private DCSet dcSet = DCSet.getInstance();
-    private String[] columnNames = Lang.getInstance().translate(new String[]{"Status", "Period", "Creator"}); //, "Data"});
-    private Boolean[] column_AutuHeight = new Boolean[]{true, false};
 
-    public PersonStatusesModel(long person_Key) {
+    public PersonStatusesModel(long personKey) {
 
-        itemKey = person_Key;
-        addObservers();
+        super(DCSet.getInstance().getPersonStatusMap(), new String[]{"Status", "Period", "Creator"},
+                new Boolean[]{true, false}, false);
 
-        statuses = dcSet.getPersonStatusMap().get(itemKey);
+        itemKey = personKey;
         statusesMap = dcSet.getItemStatusMap();
-        setRows();
+
+        addObservers();
     }
 
-
-    public TreeMap<Long, Stack<Tuple5<Long, Long, byte[], Integer, Integer>>> getSortableList() {
-        return statuses;
-    }
-
-    public String get_No_Trancaction(int row) {
+    public String getTransactionHeightSeqNo(int row) {
 
         Tuple2<Long, Tuple5<Long, Long, byte[], Integer, Integer>> value = statusesRows.get(row);
         return value.b.d + "-" + value.b.e;
     }
 
-    public Account get_Creator_Account(int row) {
+    public Account getCreator(int row) {
 
         Tuple2<Long, Tuple5<Long, Long, byte[], Integer, Integer>> value = statusesRows.get(row);
 
         int block = value.b.d;
         int recNo = value.b.e;
-        Transaction record = Transaction.findByHeightSeqNo(dcSet, block, recNo);
-        return (Account) record.getCreator();
+        Transaction transaction = Transaction.findByHeightSeqNo(dcSet, block, recNo);
+        return transaction.getCreator();
 
-    }
-
-
-// set class
-
-    public Class<? extends Object> getColumnClass(int c) {     // set column type
-        Object o = getValueAt(0, c);
-        return o == null ? Null.class : o.getClass();
-    }
-
-    // читаем колонки которые изменяем высоту
-    public Boolean[] get_Column_AutoHeight() {
-        return this.column_AutuHeight;
-    }
-
-    // устанавливаем колонки которым изменить высоту
-    public void set_get_Column_AutoHeight(Boolean[] arg0) {
-        this.column_AutuHeight = arg0;
-    }
-
-    public Stack<Tuple5<Long, Long, byte[], Integer, Integer>> getStatus(int row) {
-        return this.statuses.get(row);
-    }
-
-    @Override
-    public int getColumnCount() {
-        return this.columnNames.length;
-    }
-
-    @Override
-    public String getColumnName(int index) {
-        return this.columnNames[index];
-    }
-
-    @Override
-    public int getRowCount() {
-        return statusesRows.size();
     }
 
     @Override
     public Object getValueAt(int row, int column) {
-		/*
-		if( statuses == null || row >  statuses.size() - 1 )
-		{
-			return null;
-		}
-		
-		Long status_key_value = 0l;
-		int i = 0;
-		for ( Long status_key: statuses.keySet()) {
-			if (i++ == row)
-			{
-				status_key_value = status_key;
-				break;
-			}
-		}
-		 Stack<Tuple5<Long, Long, byte[], Integer, Integer>> entry = statuses.get(status_key_value);
-		if (entry == null || entry.isEmpty() ) return 0;
-		
-		 Tuple5<Long, Long, byte[], Integer, Integer> value = entry.peek();
-		 
-		 */
         if (statusesRows == null || row > statusesRows.size() - 1) {
             return null;
         }
@@ -193,45 +130,32 @@ public class PersonStatusesModel extends AbstractTableModel implements Observer 
         return null;
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-        try {
-            this.syncUpdate(o, arg);
-        } catch (Exception e) {
-            //GUI ERROR
-        }
-    }
-
     @SuppressWarnings("unchecked")
     public synchronized void syncUpdate(Observable o, Object arg) {
         ObserverMessage message = (ObserverMessage) arg;
 
         //CHECK IF NEW LIST
         if (message.getType() == ObserverMessage.LIST_STATUS_TYPE) {
-            if (this.statuses == null) {
-                this.statuses = (TreeMap<Long, Stack<Tuple5<Long, Long, byte[], Integer, Integer>>>) message.getValue();
-                setRows();
-            }
+
+            setRows();
 
             this.fireTableDataChanged();
-        }
-
-
-        //CHECK IF LIST UPDATED
-        if (//message.getType() == ObserverMessage.ADD_STATUS_TYPE
-            //	|| message.getType() == ObserverMessage.REMOVE_STATUS_TYPE ||
+        } else if (
                 message.getType() == ObserverMessage.ADD_PERSON_STATUS_TYPE
                         || message.getType() == ObserverMessage.REMOVE_PERSON_STATUS_TYPE)
-        //	|| message.getType() == ObserverMessage.ADD_TRANSACTION_TYPE)
         {
-            //this.statuses = (TreeMap<Long, Stack<Tuple5<Long, Long, byte[], Integer, Integer>>>) message.getValue();
-            statuses = dcSet.getPersonStatusMap().get(itemKey);
+            needUpdate = true;
+        } else if (message.getType() == ObserverMessage.GUI_REPAINT && needUpdate) {
             setRows();
-            this.fireTableDataChanged();
         }
     }
 
     public void setRows() {
+
+        needUpdate = false;
+
+        statuses = dcSet.getPersonStatusMap().get(itemKey);
+
         statusesRows = new ArrayList<Tuple2<Long, Tuple5<Long, Long, byte[], Integer, Integer>>>();
 
         for (long statusKey : statuses.keySet()) {
@@ -269,19 +193,25 @@ public class PersonStatusesModel extends AbstractTableModel implements Observer 
             Collections.sort(statusesRows, comparator);
 
         }
+
+        this.fireTableDataChanged();
+
     }
 
     public void addObservers() {
 
-        DCSet.getInstance().getPersonStatusMap().addObserver(this);
-        //Controller.getInstance().addWalletObserver(this);
+        super.addObservers();
+
+        map.addObserver(this);
 
     }
 
     public void removeObservers() {
 
-        DCSet.getInstance().getPersonStatusMap().deleteObserver(this);
-        //Controller.getInstance().deleteObserver(this);
+        super.deleteObservers();
+
+        map.deleteObserver(this);
+
     }
 
 }
