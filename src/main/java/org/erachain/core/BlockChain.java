54a0de10bec5ee1d7ee4c4e737ea527c61944f64
icreator <icreator@mail.ru>
2019-04-22 18:20:33 +0300

    fix develop

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -180,7 +180,7 @@ public class BlockChain {
     //TESTNET
     //   1486444444444l
     //	 1487844444444   1509434273     1509434273
-    public static final long DEFAULT_MAINNET_STAMP = DEVELOP_USE ? 1511164500000l : 1487844793333l;
+    public static final long DEFAULT_MAINNET_STAMP = DEVELOP_USE ? 1555944875000l : 1487844793333l;
     //public static final int FEE_MIN_BYTES = 200;
     public static final int FEE_PER_BYTE_4_10 = 64;
     public static final int FEE_PER_BYTE = 100;
