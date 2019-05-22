ae53b3b14852f3bdcedd2d8559c6145c90f5abbb
icreator <icreator@mail.ru>
2019-05-22 18:26:20 +0300

    trans search

--- src/main/java/org/erachain/database/FilteredByStringArray.java
+++ src/main/java/org/erachain/database/FilteredByStringArray.java
@@ -1,13 +1,9 @@
 package org.erachain.database;
 
-import org.erachain.utils.Pair;
-
 import java.util.List;
 
 public interface FilteredByStringArray<T> {
 
-    //abstract Pair<String, Iterable> getKeysByFilterAsArray(String filter, int offset, int limit);
-
-    abstract List<T> getKeysByFilterAsArray(String filter, int offset, int limit);
+    List<T> getKeysByFilterAsArray(String filter, int offset, int limit);
 
     }
