6bf6cfee29deb0a51b6228895b95a647d7e68ef3
icreator <icreator@mail.ru>
2019-06-02 10:46:04 +0300

    icons

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -168,7 +168,7 @@ public class BlockChain {
     public static final int AMOUNT_SCALE_FROM = DEVELOP_USE ? 1034 : 1033;
     public static final int AMOUNT_DEDAULT_SCALE = 8;
     public static final int TRADE_PRECISION = 5;
-    public static final int FREEZE_FROM = DEVELOP_USE ? 12980 : 249113;
+    public static final int FREEZE_FROM = DEVELOP_USE ? 12980 : 250000;
     // только на них можно замороженные средства вернуть из списка FOUNDATION_ADDRESSES (там же и замароженные из-за утраты)
     public static final String[] TRUE_ADDRESSES = new String[]{
             "7R2WUFaS7DF2As6NKz13Pgn9ij4sFw6ymZ"
