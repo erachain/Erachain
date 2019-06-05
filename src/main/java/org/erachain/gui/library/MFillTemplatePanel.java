a8bf31f788188959c44334c4210235759b283941
Erachain-Nikita <n.samartsev@ch-project.ru>
2019-06-05 16:40:13 +0300

    MFillTemplatePanel.java - убрал везде javax.swing. при объявлении объектов.

--- src/main/java/org/erachain/gui/library/MFillTemplatePanel.java
+++ src/main/java/org/erachain/gui/library/MFillTemplatePanel.java
@@ -20,27 +20,26 @@ import java.awt.event.ItemListener;
 import java.util.HashMap;
 import java.util.Set;
 
-public class MFillTemplatePanel extends javax.swing.JPanel {
+public class MFillTemplatePanel extends JPanel {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 1L;
     public JComboBox<TemplateCls> jComboBox_Template;
     public TemplateCls sel_Template = null;
     public JCheckBox add_Tamplate;
+
     // Variables declaration - do not modify
-    public javax.swing.JCheckBox jCheckBox_Is_Encripted;
+    public JCheckBox jCheckBox_Is_Encripted;
     public MSplitPane sp_pan;
     Params_Template_Model params_Template_Model;
     private ComboBoxModelItemsTemplates comboBoxModelTemplates;
-    private javax.swing.JCheckBox jCheckBox_Is_Text;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel_Template1;
-    private javax.swing.JScrollPane jScrollPane_Message_Public_TextPane;
-    private javax.swing.JScrollPane jScrollPane_Params_Template_Public_TextPane;
+    private JCheckBox jCheckBox_Is_Text;
+    private JLabel jLabel1;
+    private JLabel jLabel_Template1;
+    private JScrollPane jScrollPane_Message_Public_TextPane;
+    private JScrollPane jScrollPane_Params_Template_Public_TextPane;
     private MTable jTable_Params_Message_Public;
     private MImprintEDITPane jTextPane_Message_Public;
+
     public MFillTemplatePanel() {
         jTextPane_Message_Public = new MImprintEDITPane();
         comboBoxModelTemplates = new ComboBoxModelItemsTemplates();
@@ -56,72 +55,56 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
                     //		MTemplateParamTextPaneDialog d = new MTemplateParamTextPaneDialog(jTextPane_Message_Public.pars.get("{{"+ arg0.getDescription()+"}}"), getMousePosition());
                     //			str =d.tp.getText();
                 } else {
-                    str = JOptionPane.showInputDialog(MainFrame.getInstance(), Lang.getInstance().translate("Insert") + " " + arg0.getDescription().replace("!$@!", ""), jTextPane_Message_Public.pars.get("{{" + arg0.getDescription().replace("!$@!", "") + "}}"));
-
+                    str = JOptionPane.showInputDialog(MainFrame.getInstance(),
+                            Lang.getInstance().translate("Insert") + " " + arg0.getDescription().replace("!$@!", ""),
+                            jTextPane_Message_Public.pars.get("{{" + arg0.getDescription().replace("!$@!", "") + "}}"));
                 }
-                if (str == null || str.equals("")) return;
+                if (str == null || str.equals(""))
+                    return;
                 jTextPane_Message_Public.pars.replace("{{" + arg0.getDescription().replace("!$@!", "") + "}}", str);
                 jTextPane_Message_Public.init_view(jTextPane_Message_Public.text, jTextPane_Message_Public.get_Params());
-                for (int i1 = 0; i1 < params_Template_Model.getRowCount(); i1++) {
-                    if (arg0.getDescription().replace("!$@!", "").equals(params_Template_Model.getValueAt(i1, 0)))
-                        params_Template_Model.setValueAt(str, i1, 1);
-
-
+                for (int i = 0; i < params_Template_Model.getRowCount(); i++) {
+                    if (arg0.getDescription().replace("!$@!", "").equals(params_Template_Model.getValueAt(i, 0)))
+                        params_Template_Model.setValueAt(str, i, 1);
                 }
-                // System.out.print("\n"+ jTextPane_Message_Public.getText());
-
             }
-
-
         });
-
 
         initComponents();
 
         set_Template(comboBoxModelTemplates.getElementAt(0));
 
         jComboBox_Template.addItemListener(new ItemListener() {
-
             @Override
             public void itemStateChanged(ItemEvent e) {
                 // TODO Auto-generated method stub
-
-
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-
                     set_Template((TemplateCls) jComboBox_Template.getSelectedItem());
                 }
-
-
             }
         });
-
-
     }
 
     public HashMap<String, String> get_Params() {
-
         return jTextPane_Message_Public.get_Params();
-
     }
 
     public TemplateCls get_TemplateCls() {
         return (TemplateCls) jComboBox_Template.getSelectedItem();
-
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
+        GridBagConstraints gridBagConstraints;
 
-        jLabel_Template1 = new javax.swing.JLabel();
+        jLabel_Template1 = new JLabel();
         jComboBox_Template = new JComboBox<TemplateCls>(comboBoxModelTemplates);
-        jCheckBox_Is_Text = new javax.swing.JCheckBox();
-        jCheckBox_Is_Encripted = new javax.swing.JCheckBox();
+        jCheckBox_Is_Text = new JCheckBox();
+        jCheckBox_Is_Encripted = new JCheckBox();
         sp_pan = new MSplitPane();
-        jScrollPane_Message_Public_TextPane = new javax.swing.JScrollPane();
-        jScrollPane_Params_Template_Public_TextPane = new javax.swing.JScrollPane();
+        jScrollPane_Message_Public_TextPane = new JScrollPane();
+        jScrollPane_Params_Template_Public_TextPane = new JScrollPane();
 
         params_Template_Model = new Params_Template_Model();
         jTable_Params_Message_Public = new MTable(params_Template_Model);
@@ -149,9 +132,9 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
         jTable_Params_Message_Public.setVisible(false);
         add_Tamplate = new JCheckBox();
 
-        jLabel1 = new javax.swing.JLabel();
+        jLabel1 = new JLabel();
 
-        setLayout(new java.awt.GridBagLayout());
+        setLayout(new GridBagLayout());
 
         add_Tamplate.setText(Lang.getInstance().translate("Insert template") + "?");
         add_Tamplate.addActionListener(new ActionListener() {
@@ -166,7 +149,6 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
                                                    set_Template((TemplateCls) jComboBox_Template.getSelectedItem());
                                                } else {
                                                    sel_Template = null;
-
                                                }
 
                                            }
@@ -175,44 +157,44 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
                                        }
         );
 
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
-        gridBagConstraints.insets = new java.awt.Insets(8, 8, 8, 0);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+        gridBagConstraints.insets = new Insets(8, 8, 8, 0);
         add(add_Tamplate, gridBagConstraints);
 
         jLabel_Template1.setText(Lang.getInstance().translate("Select template") + ":");
-        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
-        gridBagConstraints.insets = new java.awt.Insets(8, 8, 8, 0);
+        gridBagConstraints.anchor = GridBagConstraints.LINE_END;
+        gridBagConstraints.insets = new Insets(8, 8, 8, 0);
         add(jLabel_Template1, gridBagConstraints);
 
 
-        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridwidth = 4;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_START;
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
         gridBagConstraints.weightx = 0.1;
-        gridBagConstraints.insets = new java.awt.Insets(8, 0, 8, 0);
+        gridBagConstraints.insets = new Insets(8, 0, 8, 0);
         add(jComboBox_Template, gridBagConstraints);
 
         jCheckBox_Is_Text.setText("jCheckBox1");
-        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
-        gridBagConstraints.insets = new java.awt.Insets(0, 8, 8, 0);
+        gridBagConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.insets = new Insets(0, 8, 8, 0);
         //	add(jCheckBox_Is_Text, gridBagConstraints);
 
         jCheckBox_Is_Encripted.setText(Lang.getInstance().translate("Encrypt message"));
-        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
-        gridBagConstraints.insets = new java.awt.Insets(0, 0, 8, 0);
+        gridBagConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.insets = new Insets(0, 0, 8, 0);
 //		add(jCheckBox_Is_Encripted, gridBagConstraints);
 
         jScrollPane_Message_Public_TextPane.setViewportView(jTextPane_Message_Public);
@@ -225,20 +207,20 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
 
         sp_pan.setRightComponent(jScrollPane_Params_Template_Public_TextPane);
 
-        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.gridwidth = 7;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
         gridBagConstraints.weightx = 0.2;
         gridBagConstraints.weighty = 0.2;
-        gridBagConstraints.insets = new java.awt.Insets(0, 8, 8, 8);
+        gridBagConstraints.insets = new Insets(0, 8, 8, 8);
         add(sp_pan, gridBagConstraints);
 
         jLabel1.setText("                   ");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.insets = new java.awt.Insets(8, 0, 8, 8);
+        gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.insets = new Insets(8, 0, 8, 8);
         add(jLabel1, gridBagConstraints);
     }// </editor-fold>
     // End of variables declaration
@@ -265,22 +247,14 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
         for (String s : sk) {
             ss.get(s);
             params_Template_Model.addRow(new Object[]{s, ss.get(s)});
-
         }
-
-
     }
 
     class Params_Template_Model extends DefaultTableModel {
-
-        /**
-         *
-         */
         private static final long serialVersionUID = 1L;
 
         public Params_Template_Model() {
             super(new Object[]{Lang.getInstance().translate("Name"), Lang.getInstance().translate("=")}, 0);
-
         }
 
         public int getColumnCount() {
@@ -299,14 +273,11 @@ public class MFillTemplatePanel extends javax.swing.JPanel {
         }
 
         public Object getValueAt(int row, int col) {
-
-
-            if (this.getRowCount() < row || this.getRowCount() == 0 || col < 0 || row < 0) return null;
+            if (this.getRowCount() < row ||
+                    this.getRowCount() == 0 ||
+                    col < 0 || row < 0)
+                return null;
             return super.getValueAt(row, col);
-
-
         }
-
-
     }
 }
