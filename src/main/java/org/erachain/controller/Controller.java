7dccfa560588328bf8967c6abff656bb245672c9
Pavel S <p.sergeev@ch-group.ru>
2019-06-10 09:38:37 +0300

    Версия 4.11.15

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -93,8 +93,8 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "4.11.14 beta dev";
-    public static String buildTime = "2019-05-17 13:33:33 UTC";
+    public static String version = "4.11.15 beta dev";
+    public static String buildTime = "2019-06-10 13:33:33 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
     public static final char GROUPING_SEPARATOR = '`';
