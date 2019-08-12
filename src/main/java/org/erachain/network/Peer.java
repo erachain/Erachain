1166d3b6698f09f1fa92b9c6e87b84adcb6c93db
icreator <icreator@mail.ru>
2019-08-12 14:14:56 +0300

    QUEUE_LENGTH in Sender

--- src/main/java/org/erachain/network/Peer.java
+++ src/main/java/org/erachain/network/Peer.java
@@ -34,7 +34,7 @@ public class Peer extends MonitoredThread {
     static Logger LOGGER = LoggerFactory.getLogger(Peer.class.getName());
     // Слишком бльшой буфер позволяет много посылок накидать не ожидая их приема. Но запросы с возратом остаются в очереди на долго
     // поэтому нужно ожидание дольще делать
-    private static int SOCKET_BUFFER_SIZE = 1024 << (8 + Controller.HARD_WORK);
+    private static int SOCKET_BUFFER_SIZE = 1024 << (10 + Controller.HARD_WORK);
     private static int MAX_BEFORE_PING = SOCKET_BUFFER_SIZE >> 2;
     public Network network;
     private InetAddress address;
