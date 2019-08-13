6b6579993873f2262a20048b23fbafb6f768cace
icreator <icreator@mail.ru>
2019-08-13 11:04:36 +0300

    fix long minus

--- src/main/java/org/erachain/core/BlockBuffer.java
+++ src/main/java/org/erachain/core/BlockBuffer.java
@@ -74,7 +74,7 @@ public class BlockBuffer extends Thread {
                     // время ожидания увеличиваем по мере номера блока - он ведь на той тсроне синхронно нам будет посылаться
                     long timeSOT = Synchronizer.GET_BLOCK_TIMEOUT + i * (long)(Synchronizer.GET_BLOCK_TIMEOUT >> 2)
                             + currentTimestamp - System.currentTimeMillis();
-                    if (timeSOT > 600000) {
+                    if (timeSOT > 600000 || timeSOT < 1) {
                         timeSOT = 600000;
                     }
                     this.loadBlock(signature, timeSOT);
