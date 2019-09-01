5f212c4e5ec49e75a67b775eef24fa5652c22fb1
icreator <icreator@mail.ru>
2019-09-01 20:12:58 +0300

    peers

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -42,7 +42,7 @@ public class BlockChain {
     /**
      * set uo all balabces ERA to 10000 and COMPU to 100
      */
-    public static final boolean ERA_COMPU_ALL_UP = false;
+    public static final boolean ERA_COMPU_ALL_UP = true;
 
     static final public int CHECK_BUGS = 1;
 
