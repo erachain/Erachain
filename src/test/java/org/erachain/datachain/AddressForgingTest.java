1deb06e6aa7b69d75644994166a329a3e4ea1f65
icreator <icreator@mail.ru>
2019-09-01 13:39:30 +0300

    tests

---
+++ src/test/java/org/erachain/datachain/AddressForgingTest.java
@@ -0,0 +1,28 @@
+package org.erachain.datachain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddressForgingTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void set() {
+    }
+
+    @Test
+    public void testSet() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+}
