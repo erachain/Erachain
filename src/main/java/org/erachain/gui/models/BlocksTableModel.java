af22aa75864b774a28d96b5dedc4998a228198b3
icreator <icreator@mail.ru>
2019-09-01 23:25:37 +0300

    gui

--- src/main/java/org/erachain/gui/models/BlocksTableModel.java
+++ src/main/java/org/erachain/gui/models/BlocksTableModel.java
@@ -50,7 +50,6 @@ public class BlocksTableModel extends TimerTableModelCls<Block.BlockHead> {
                 return null;
             }
             Block.BlockHead block = list.get(row);
-            Tuple2<Integer, Integer> forgingPoint = block.creator.getForgingData(DCSet.getInstance(), block.heightBlock);
             switch (column) {
                 case COLUMN_HEIGHT:
                     return block.heightBlock + "";
@@ -61,15 +60,23 @@ public class BlocksTableModel extends TimerTableModelCls<Block.BlockHead> {
                 case COLUMN_GENERATOR:
                     return block.creator.getPersonAsString();
                 case COLUMN_GB:
-                    if (block.target == 0) {
+                    if (block.target == 0 || block.heightBlock == 1) {
                         return "GENESIS";
                     }
+                    Tuple2<Integer, Integer> forgingPoint = block.creator.getForgingData(DCSet.getInstance(), block.heightBlock);
                     return forgingPoint.b + " ";
                 case COLUMN_DH:
+                    if (block.target == 0 || block.heightBlock == 1) {
+                        return "GENESIS";
+                    }
+                    forgingPoint = block.creator.getForgingData(DCSet.getInstance(), block.heightBlock);
                     return (block.heightBlock - forgingPoint.a) + "";
                 case COLUMN_WV:
                     return block.winValue + "";
                 case COLUMN_dtWV:
+                    if (block.target == 0 || block.heightBlock == 1) {
+                        return "GENESIS";
+                    }
                     return String.format("%10.3f%%", (100f * (block.winValue - block.target) / block.target));
                 case COLUMN_TRANSACTIONS:
                     return block.transactionsCount;
