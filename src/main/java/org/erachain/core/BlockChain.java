8e2513178875f135097852bbb5ce2dea94bbe900
icreator <icreator@mail.ru>
2019-09-03 12:38:43 +0300

    сдвинул UNCONFIRMED_SORT_WAIT_MS
    там уже есть трнзакции в этом диаппазоне

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -34,7 +34,7 @@ public class BlockChain {
     //public static final int START_LEVEL = 1;
 
     public static final int TESTS_VERS = 0; // not use TESTs - or 411 (as version)
-    public static final boolean DEVELOP_USE = true;
+    public static final boolean DEVELOP_USE = false;
 
     public static final int BLOCK_COUNT = 0; ////
     static final public boolean TEST_DB_TXS_OFF = false;
@@ -88,7 +88,7 @@ public class BlockChain {
     static final int WIN_TIMEPOINT = GENERATING_MIN_BLOCK_TIME_MS >> 2;
     public static final int WIN_BLOCK_BROADCAST_WAIT_MS = 10000; //
     // задержка на включение в блок для хорошей сортировки
-    public static final int UNCONFIRMED_SORT_WAIT_MS = DEVELOP_USE? 5000: 5000;
+    public static final int UNCONFIRMED_SORT_WAIT_MS = DEVELOP_USE? -5000: -5000;
     public static final int CHECK_PEERS_WEIGHT_AFTER_BLOCKS = 1; // проверить наше цепочку по силе с окружающими
     // хранить неподтвержденные долше чем то время когда мы делаем обзор цепочки по силе
     public static final int UNCONFIRMED_DEADTIME_MS = DEVELOP_USE? GENERATING_MIN_BLOCK_TIME_MS << 4 : GENERATING_MIN_BLOCK_TIME_MS << 3;
