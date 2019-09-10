b0e5231bdd21a16fcaba99243a9a15b23ec7b137
icreator <icreator@mail.ru>
2019-09-10 17:56:46 +0300

    RocksDB

--- src/main/java/org/erachain/datachain/ItemAssetBalanceRocksMap.java
+++ src/main/java/org/erachain/datachain/ItemAssetBalanceRocksMap.java
@@ -2,19 +2,26 @@ package org.erachain.datachain;
 
 import com.google.common.primitives.Bytes;
 import com.google.common.primitives.Longs;
+import org.erachain.controller.Controller;
+import org.erachain.core.account.Account;
+import org.erachain.database.SortableList;
 import org.erachain.dbs.rocksDB.DCMap;
+import org.erachain.dbs.rocksDB.integration.DBMapDB;
+import org.mapdb.BTreeMap;
 import org.mapdb.DB;
 import org.mapdb.Fun;
 
 import java.io.File;
 import java.math.BigDecimal; // org.erachain.dbs.rocksDB.DBMap
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static org.erachain.dbs.rocksDB.utils.ConstantsRocksDB.ROCKS_DB_FOLDER;
 
-public class ItemAssetBalanceRocksMap extends org.erachain.dbs.rocksDB.DCMap<byte[], Fun.Tuple5<
+public class ItemAssetBalanceRocksMap extends DCMap<byte[], Fun.Tuple5<
         Fun.Tuple2<BigDecimal, BigDecimal>, // in OWN - total INCOMED + BALANCE
         Fun.Tuple2<BigDecimal, BigDecimal>, // in DEBT
         Fun.Tuple2<BigDecimal, BigDecimal>, // in STOCK
@@ -27,6 +34,9 @@ public class ItemAssetBalanceRocksMap extends org.erachain.dbs.rocksDB.DCMap<byt
     private List<org.erachain.dbs.rocksDB.indexes.IndexDB> indexes;
     private org.erachain.dbs.rocksDB.integration.DBRocksDBTable rocksDBTable;
 
+    @SuppressWarnings("rawtypes")
+    private BTreeMap assetKeyMap;
+    private BTreeMap addressKeyMap;
 
     public ItemAssetBalanceRocksMap(DCSet databaseSet, DB database) {
         super(databaseSet, database);
@@ -34,6 +44,10 @@ public class ItemAssetBalanceRocksMap extends org.erachain.dbs.rocksDB.DCMap<byt
 
     public ItemAssetBalanceRocksMap(ItemAssetBalanceRocksMap parent) {
         super(parent, null);
+    }
+
+    @Override
+    protected void createIndexes(DB database) {
     }
 
     @Override
@@ -48,6 +62,11 @@ public class ItemAssetBalanceRocksMap extends org.erachain.dbs.rocksDB.DCMap<byt
 
         databaseSet.addExternalMaps(this);
 
+    }
+
+    @Override
+    protected void getMemoryMap() {
+        rocksDBTable = new DBMapDB<>(new HashMap<>());
     }
 
     /*
@@ -66,6 +85,148 @@ public class ItemAssetBalanceRocksMap extends org.erachain.dbs.rocksDB.DCMap<byt
         return value;
     }
     */
+
+
+    @Override
+    protected Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> getDefaultValue() {
+        return new Fun.Tuple5<
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>>
+                (new Fun.Tuple2<BigDecimal, BigDecimal>(BigDecimal.ZERO, BigDecimal.ZERO),
+                        new Fun.Tuple2<BigDecimal, BigDecimal>(BigDecimal.ZERO, BigDecimal.ZERO),
+                        new Fun.Tuple2<BigDecimal, BigDecimal>(BigDecimal.ZERO, BigDecimal.ZERO),
+                        new Fun.Tuple2<BigDecimal, BigDecimal>(BigDecimal.ZERO, BigDecimal.ZERO),
+                        new Fun.Tuple2<BigDecimal, BigDecimal>(BigDecimal.ZERO, BigDecimal.ZERO));
+    }
+
+	/*
+	public void set(byte[] address, BigDecimal value)
+	{
+		this.set(address, FEE_KEY, value);
+	}
+	 */
+
+    public long getAssetKeyFromKey(byte[] key) {
+        // ASSET KEY
+        byte[] assetKeyBytes = new byte[8];
+        System.arraycopy(key, 20, assetKeyBytes, 0, 8);
+        return Longs.fromByteArray(assetKeyBytes);
+    }
+
+    public byte[] getShortAccountFromKey(byte[] key) {
+        // ASSET KEY
+        byte[] shortAddressBytes = new byte[20];
+        System.arraycopy(key, 0, shortAddressBytes, 0, 20);
+        return shortAddressBytes;
+
+    }
+
+    public void set(byte[] address, long key, Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> value) {
+        if (key < 0)
+            key = -key;
+
+        this.set(Bytes.concat(address, Longs.toByteArray(key)), value);
+    }
+
+    private Account testAcc = new Account("76ACGgH8c63VrrgEw1wQA4Dno1JuPLTsWe");
+    public boolean set(byte[] key, Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> value) {
+
+        boolean test = false;
+        if (testAcc.equals(key)) {
+            test = true;
+        }
+
+        boolean result = super.set(key, value);
+
+        if (test) {
+            Fun.Tuple5 balance5 = get(key);
+        }
+
+        return result;
+
+    }
+
+	/*
+	public BigDecimal get(byte[] address)
+	{
+		return this.get(address, FEE_KEY);
+	}
+	 */
+
+    public Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> get(byte[] address, long key) {
+        if (key < 0)
+            key = -key;
+
+
+        Fun.Tuple5<
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> value = this.get(
+                Bytes.concat(address, Longs.toByteArray(key)));
+
+		/*
+		// TODO for TEST
+		// FOR TEST NET
+		if (key == Transaction.FEE_KEY &&
+				value.a.compareTo(BigDecimal.ONE) < 0) {
+
+			return new Tuple3<BigDecimal, BigDecimal, BigDecimal>(
+					BigDecimal.ONE, BigDecimal.ZERO, BigDecimal.ZERO);
+
+		}
+		 */
+
+        return value;
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public SortableList<byte[], Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>>> getBalancesSortableList(long key) {
+
+        if (Controller.getInstance().onlyProtocolIndexing)
+            return null;
+
+        if (key < 0)
+            key = -key;
+
+        //FILTER ALL KEYS
+        Collection<byte[]> keys = this.assetKeyMap.subMap(
+                Fun.t2(key, null),
+                Fun.t2(key, Fun.HI())).values();
+
+        int tt = keys.size();
+        //RETURN
+        return new SortableList<byte[], Fun.Tuple5<
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>>>(this, keys);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public SortableList<byte[], Fun.Tuple5<
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+            Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>>> getBalancesSortableList(Account account) {
+
+        if (Controller.getInstance().onlyProtocolIndexing)
+            return null;
+
+        //FILTER ALL KEYS
+        Collection<byte[]> keys = this.addressKeyMap.subMap(
+                Fun.t2(account.getAddress(), null),
+                Fun.t2(account.getAddress(), Fun.HI())).values();
+
+        int tt = keys.size();
+        //RETURN
+        return new SortableList<byte[], Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>,
+                Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>>>(this, keys);
+    }
 
     @Override
     public void reset() {
