15ebdbb02e0a7207afdf54e1d11bb7438d03e508
icreator <icreator@mail.ru>
2019-09-11 09:56:03 +0300

    RocksDB

--- src/main/java/org/erachain/dbs/rocksDB/utils/ConstantsRocksDB.java
+++ src/main/java/org/erachain/dbs/rocksDB/utils/ConstantsRocksDB.java
@@ -1,7 +1,11 @@
 package org.erachain.dbs.rocksDB.utils;
 
+import org.erachain.settings.Settings;
+
+import java.io.File;
+
 public class ConstantsRocksDB {
-    public static String ROCKS_DB_FOLDER = "./RocksDB";
+    public static String ROCKS_DB_FOLDER = Settings.getInstance().getDataDir() + File.separator + "RocksDB";
     public static String ROCKS_DB_WALLET_FOLDER = "./RocksDBWallet";
     public static int SIZE_ADDRESS = 20;
 
