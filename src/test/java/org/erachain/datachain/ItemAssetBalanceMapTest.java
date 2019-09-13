b1fb656e5b91db232f9732e4e7d9111a497146bb
icreator <icreator@mail.ru>
2019-09-13 17:33:38 +0300

    tests getShortAccountFromKey

---
+++ src/test/java/org/erachain/datachain/ItemAssetBalanceMapTest.java
@@ -0,0 +1,48 @@
+package org.erachain.datachain;
+
+import org.erachain.core.account.Account;
+import org.junit.Test;
+import org.mapdb.Fun;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class ItemAssetBalanceMapTest {
+
+    DCSet dcSet;
+    String address = "7CzxxwH7u9aQtx5iNHskLQjyJvybyKg8rF";
+    BigDecimal balA = new BigDecimal("0.1");
+    BigDecimal balB = new BigDecimal("0.2");
+    Fun.Tuple2<BigDecimal, BigDecimal> balAB = new Fun.Tuple2<>(balA, balB);
+    ItemAssetBalanceMap map;
+
+    Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balance;
+    Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balance2;
+
+    private void init() {
+
+        dcSet = DCSet.createEmptyDatabaseSet();
+        map = dcSet.getAssetBalanceMap();
+
+        balance = new Fun.Tuple5<>(balAB, balAB, balAB, balAB, balAB);
+
+
+    }
+
+    @Test
+    public void set() {
+
+        Account account = new Account("73EotEbxvAo39tyugJSyL5nbcuMWs4aUpS");
+        map.set(account.getShortAddressBytes(), 2L, balance);
+
+        balance2 = map.get(account.getShortAddressBytes(), 2L);
+
+        Account account2 = new Account(map.getShortAccountFromKey(account.getShortAddressBytes()));
+        assertEquals(account.getAddress(), account2.getAddress());
+
+    }
+
+}
