d33f430b1bc6a5c29bfea3d8b109e1f94cec5eb4
icreator <icreator@mail.ru>
2019-09-13 15:58:13 +0300

    good!

--- src/main/java/org/erachain/dbs/rocksDB/indexes/indexByteables/IndexByteableTuple3StringLongInteger.java
+++ src/main/java/org/erachain/dbs/rocksDB/indexes/indexByteables/IndexByteableTuple3StringLongInteger.java
@@ -1,18 +1,18 @@
 package org.erachain.dbs.rocksDB.indexes.indexByteables;
 
-import org.apache.flink.api.java.tuple.Tuple3;
 import org.erachain.dbs.rocksDB.indexes.IndexByteable;
 import org.erachain.dbs.rocksDB.transformation.ByteableInteger;
 import org.erachain.dbs.rocksDB.transformation.ByteableLong;
 import org.erachain.dbs.rocksDB.transformation.ByteableString;
+import org.mapdb.Fun;
 
-public class IndexByteableTuple3StringLongInteger implements IndexByteable<Tuple3<String, Long, Integer>, Long> {
+public class IndexByteableTuple3StringLongInteger implements IndexByteable<Fun.Tuple3<String, Long, Integer>, Long> {
     @Override
-    public byte[] toBytes(Tuple3<String, Long, Integer> result, Long key) {
+    public byte[] toBytes(Fun.Tuple3<String, Long, Integer> result, Long key) {
         return org.bouncycastle.util.Arrays.concatenate(
-                new ByteableString().toBytesObject(result.f0),
-                new ByteableLong().toBytesObject(result.f1),
-                new ByteableInteger().toBytesObject(result.f2)
+                new ByteableString().toBytesObject(result.a),
+                new ByteableLong().toBytesObject(result.b),
+                new ByteableInteger().toBytesObject(result.c)
         );
     }
 }
