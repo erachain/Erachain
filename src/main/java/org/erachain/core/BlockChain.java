35266176dcd26722a76342b1c5a77c6c5086bb0e
icreator <icreator@mail.ru>
2019-10-11 18:13:53 +0300

    fix setColumnFamilyHandle

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -40,7 +40,7 @@ public class BlockChain {
 
     public static final int BLOCK_COUNT = 0; ////
     // сколько трназакции в блоке - если больше 0 то запускает тест на старте
-    public static final int TEST_DB = 100000;
+    public static final int TEST_DB = 10000;
     public static final boolean STOP_GENERATE_BLOCKS = false;
 
     // размер балансового поля - чем больше тем сложнее
