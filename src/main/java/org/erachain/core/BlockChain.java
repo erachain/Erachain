709774c9bf0a6cc49defc3b70dcbc50d8c372651
icreator <icreator@mail.ru>
2019-10-23 15:12:25 +0300

    getCheckPoint = 1

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -36,7 +36,7 @@ public class BlockChain {
     //public static final int START_LEVEL = 1;
 
     public static final int TESTS_VERS = 0; // not use TESTs - or 411 (as version)
-    public static final boolean DEVELOP_USE = false;
+    public static final boolean DEVELOP_USE = true;
 
     public static final int BLOCK_COUNT = 0; ////
     // сколько транзакции в блоке - если больше 0 то запускает тест на старте
