9b11046e244d895e6bcd6ee9966fb5bc7305d2c5
icreator <icreator@mail.ru>
2019-12-09 17:32:51 +0300

    ordersbyaddress API

--- src/main/java/org/erachain/core/blockexplorer/BlockExplorer.java
+++ src/main/java/org/erachain/core/blockexplorer/BlockExplorer.java
@@ -1867,7 +1867,7 @@ public class BlockExplorer {
             output.put("released", "--");
             output.put("assetName", "--");
             output.put("Label_Title", (Lang.getInstance().translateFromLangObj("Top %limit% %assetName% Richest", langObj)
-                    .replace("%limit%", String.valueOf(limit))).replace("%assetName%", "--"));
+                    .replace("%limit%", String.valueOf(limit > 0 ? limit : ""))).replace("%assetName%", "--"));
             output.put("Label_All_non",
                     (Lang.getInstance().translateFromLangObj("All non-empty %assetName% accounts (%count%)", langObj)
                             .replace("%assetName%", "--")).replace("%count%", String.valueOf(couter)));
@@ -1883,7 +1883,7 @@ public class BlockExplorer {
             output.put("released", asset.getReleased(dcSet).toPlainString());
             output.put("assetName", asset.getName());
             output.put("Label_Title", (Lang.getInstance().translateFromLangObj("Top %limit% %assetName% Richest", langObj)
-                    .replace("%limit%", String.valueOf(limit))).replace("%assetName%", asset.getName()));
+                    .replace("%limit%", String.valueOf(limit > 0 ? limit : ""))).replace("%assetName%", asset.getName()));
             output.put("Label_All_non",
                     (Lang.getInstance().translateFromLangObj("All non-empty %assetName% accounts (%count%)", langObj)
                             .replace("%assetName%", asset.getName())).replace("%count%", String.valueOf(couter)));
