1410d7202ed3f401747eef26ac40491c472fedb8
icreator <icreator@mail.ru>
2020-01-06 11:35:00 +0300

    MarkDown td

--- src/main/java/org/erachain/datachain/IssuePersonMap.java
+++ src/main/java/org/erachain/datachain/IssuePersonMap.java
@@ -1,11 +1,7 @@
 package org.erachain.datachain;
 
-import org.erachain.core.BlockChain;
-import org.erachain.core.crypto.Base58;
 import org.erachain.core.item.ItemCls;
 import org.mapdb.DB;
-
-import java.util.Arrays;
 
 /**
  * see datachain.IssueItemMap
@@ -19,26 +15,6 @@ public class IssuePersonMap extends IssueItemMap {
 
     public IssuePersonMap(IssuePersonMap parent, DCSet dcSet) {
         super(parent, dcSet);
-    }
-
-    /////
-    byte[] checkSign = Base58.decode("3gK1L3pLsRCUbyXbfqDujTrsaZGWocBT58easGe1Gy6fgt43UMbULkX3C46utuWyVStwivisZxQzeiv2HEjB4r7h");
-    @Override
-    public void delete(byte[] signature) {
-        if(BlockChain.CHECK_BUGS > 3
-                && Arrays.equals(checkSign, signature)) {
-            boolean debug = true;
-        }
-        super.delete(signature);
-    }
-
-    @Override
-    public Long remove(byte[] signature) {
-        if(BlockChain.CHECK_BUGS > 3
-                && Arrays.equals(checkSign, signature)) {
-            boolean debug = true;
-        }
-        return super.remove(signature);
     }
 
 }
