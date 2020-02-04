3f558b59e58ceb37a8622f30fdc04479983df86d
icreator <icreator@mail.ru>
2020-02-04 13:59:00 +0300

    убрал включение новой логики наград и прав га внесение персон

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -1067,7 +1067,7 @@ public class BlockChain {
             return this.genesisTimestamp + (long) height * GENERATING_MIN_BLOCK_TIME_MS(height);
         }
 
-        return this.genesisTimestamp + 46667L
+        return this.genesisTimestamp + (DEVELOP_USE ? 0L : 16667L)
                 + (long) VERS_30SEC * GENERATING_MIN_BLOCK_TIME_MS(VERS_30SEC)
                 + (long) (height - VERS_30SEC) * GENERATING_MIN_BLOCK_TIME_MS(height);
 
