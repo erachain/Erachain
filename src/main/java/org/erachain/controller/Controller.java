c1df9e286aae411e7b6192d02d439ceb12143dd1
icreator <icreator@mail.ru>
2020-04-09 18:01:34 +0300

    убраны лишние пробелы

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -93,7 +93,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "4.23.01 DEV";
+    public static String version = "4.23.01";
     public static String buildTime = "2020-04-02 13:33:33 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
