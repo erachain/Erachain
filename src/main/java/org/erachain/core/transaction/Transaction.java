5601f8dae34c09be9c7dc97dca87b919099f7c59
icreator <icreator@mail.ru>
2020-05-11 15:02:19 +0300

    isOutsideType

--- src/main/java/org/erachain/core/transaction/Transaction.java
+++ src/main/java/org/erachain/core/transaction/Transaction.java
@@ -106,7 +106,6 @@ public abstract class Transaction implements ExplorerJsonLine {
     // ASSETS
     public static final int INVALID_QUANTITY = 30;
 
-    // public static final int ASSET_DOES_NOT_EXIST = 31;
     public static final int INVALID_AMOUNT_IS_NULL = 31;
     public static final int NEGATIVE_AMOUNT = 32;
     public static final int INVALID_AMOUNT = 33;
@@ -129,6 +128,8 @@ public abstract class Transaction implements ExplorerJsonLine {
     public static final int NO_INCLAIM_BALANCE = 49;
 
     public static final int HASH_ALREDY_EXIST = 51;
+
+    public static final int INVALID_CLAIM_DEBT_CREATOR = 61;
 
     public static final int NOT_ENOUGH_ERA_OWN_10 = 101;
     public static final int NOT_ENOUGH_ERA_USE_10 = 102;
