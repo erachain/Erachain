91dd1ee9c17e1fc6fc96ca6a670bae5c222fa142
icreator <icreator@mail.ru>
2020-07-06 09:58:03 +0300

    WRONG_SIGNER RSignNote

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -90,7 +90,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "5.0.01 DEV";
+    public static String version = "5.0.01 DEV 01";
     public static String buildTime = "2020-05-06 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
