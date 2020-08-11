caa1e23d2f2997ce20e584fddf5e7305df3f8e8a
icreator <icreator@mail.ru>
2020-08-11 18:48:33 +0300

    fix UNION GUI

--- src/main/java/org/erachain/gui/models/RendererIcon.java
+++ src/main/java/org/erachain/gui/models/RendererIcon.java
@@ -9,33 +9,6 @@ import javax.swing.table.DefaultTableCellRenderer;
 public class RendererIcon extends DefaultTableCellRenderer {
     private static final long serialVersionUID = 1L;
 
-    /*
-
-    тут при выделении не перекрашивает
-
-    @Override
-    public Component getTableCellRendererComponent(JTable table,
-                                                   Object value,
-                                                   boolean isSelected,
-                                                   boolean hasFocus,
-                                                   int row,
-                                                   int column) {
-        // Get icon to use for the list item value
-        ItemCls item = (ItemCls)value;
-        byte[] iconBytes = item.getIcon();
-        if (iconBytes != null && iconBytes.length > 0) {
-            ImageIcon image = new ImageIcon(iconBytes);
-            Image Im = image.getImage().getScaledInstance(20, 20, 1);
-            setIcon(new ImageIcon(Im));
-        }
-
-        setText(item.viewName());
-
-        return this;
-
-    }
-    */
-
     @Override
     protected void setValue(Object value) {
 
