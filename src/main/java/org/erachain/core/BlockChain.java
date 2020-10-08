237f3129ef3571fcee35548d815df0508e320bb9
icreator <icreator@mail.ru>
2020-10-08 16:58:04 +0300

    fix REPEAT_WIN

--- src/main/java/org/erachain/core/BlockChain.java
+++ src/main/java/org/erachain/core/BlockChain.java
@@ -1129,7 +1129,7 @@ public class BlockChain {
 
         if (forgingBalance < BlockChain.MIN_GENERATING_BALANCE) {
             if (height > ALL_BALANCES_OK_TO)
-                return 0l;
+                return 0L;
             forgingBalance = BlockChain.MIN_GENERATING_BALANCE;
         }
 
@@ -1141,7 +1141,7 @@ public class BlockChain {
 
         int repeatsMin;
 
-        if (height < BlockChain.REPEAT_WIN) {
+        if (height <= BlockChain.REPEAT_WIN) {
             repeatsMin = height - 2;
         } else {
             repeatsMin = BlockChain.GENESIS_ERA_TOTAL / forgingBalance;
