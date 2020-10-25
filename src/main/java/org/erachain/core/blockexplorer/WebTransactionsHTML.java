d6c201e3677735e41a0f423c611206866155e6b2
icreator <icreator@mail.ru>
2020-10-25 13:48:30 +0300

    ExLinksMap Tuple3

--- src/main/java/org/erachain/core/blockexplorer/WebTransactionsHTML.java
+++ src/main/java/org/erachain/core/blockexplorer/WebTransactionsHTML.java
@@ -823,7 +823,7 @@ public class WebTransactionsHTML {
                     out += "<h2>" + Lang.getInstance().translateFromLangObj("Appendix", langObj)
                             + "</h2><h3>" + childTx.getTitle() + "</h3>";
                     out += "<a href=?tx=" + childTx.viewHeightSeq() + BlockExplorer.get_Lang(langObj) + ">"
-                            + childTx.viewType() + " " + childTx.viewHeightSeq() + "</a> "
+                            + childTx.viewFullTypeName() + " " + childTx.viewHeightSeq() + "</a> "
                             + " " + DateTimeFormat.timestamptoString(childTx.getTimestamp()) + " ";
                     out += "<a href=?address="
                             + childTx.getCreator().getAddress() + BlockExplorer.get_Lang(langObj) + "><b>" + childTx.getCreator().getPersonAsString()
@@ -838,7 +838,7 @@ public class WebTransactionsHTML {
                         out += "<h2>" + Lang.getInstance().translateFromLangObj("Appendix", langObj) + " " + ++count
                                 + "</h2><h3>" + childTx.getTitle() + "</h3>";
                         out += "<a href=?tx=" + childTx.viewHeightSeq() + BlockExplorer.get_Lang(langObj) + ">"
-                                + childTx.viewHeightSeq() + "</a> "
+                                + childTx.viewFullTypeName() + " " + childTx.viewHeightSeq() + "</a> "
                                 + " " + DateTimeFormat.timestamptoString(childTx.getTimestamp()) + " ";
                         out += "<a href=?address="
                                 + childTx.getCreator().getAddress() + BlockExplorer.get_Lang(langObj) + "><b>" + childTx.getCreator().getPersonAsString()
