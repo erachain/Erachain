3f9dcbd1f50f00148a769da5818c02f6f43b5240
icreator <icreator@mail.ru>
2020-11-27 13:36:38 +0300

    vers

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -95,7 +95,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "5.1.01";
+    public static String version = "5.1.01.02";
     public static String buildTime = "2020-10-09 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
