580bbade25d0d29a7e13a41b0feb75c475fba8fb
icreator <icreator@mail.ru>
2021-01-03 17:36:32 +0300

    viewName key

--- src/main/java/org/erachain/gui/models/WalletOrdersTableModel.java
+++ src/main/java/org/erachain/gui/models/WalletOrdersTableModel.java
@@ -55,14 +55,14 @@ public class WalletOrdersTableModel extends WalletTableModel<Order> implements O
 
             case COLUMN_HAVE:
                 AssetCls asset = DCSet.getInstance().getItemAssetMap().get(order.getHaveAssetKey());
-                return asset == null ? "[" + order.getHaveAssetKey() + "]" : asset.getShort();
+                return asset == null ? asset.toString();
 
             case COLUMN_PRICE:
                 return order.getPrice();
 
             case COLUMN_WANT:
                 asset = DCSet.getInstance().getItemAssetMap().get(order.getWantAssetKey());
-                return asset == null ? "[" + order.getWantAssetKey() + "]" : asset.getShort();
+                return asset == null ? asset.toString();
 
             case COLUMN_AMOUNT_WANT:
                 return order.getAmountWant().toPlainString();
