7f90694a9cf0624d0331b1a953496cd9e15ec5a0
icreator <icreator@mail.ru>
2021-01-20 19:04:22 +0300

    fix useSelfBalance

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -95,7 +95,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "5.2.01 dev";
+    public static String version = "5.2.0.01";
     public static String buildTime = "2020-12-28 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
