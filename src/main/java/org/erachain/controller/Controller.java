48cc3a07a8dfd8233f7b4ff6bf8a2bd500918cc9
icreator <icreator@mail.ru>
2021-03-11 17:47:09 +0300

    NFT

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -96,7 +96,7 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "5.2.02.01";
+    public static String version = "5.2.03 DEV";
     public static String buildTime = "2021-03-01 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
