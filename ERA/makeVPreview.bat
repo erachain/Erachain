1e19659dc737277500b622bf5f62c4160f841e64
icreator <icreator@mail.ru>
2021-05-20 17:35:33 +0300

    ffmpeg

--- ERA/makeVPreview.bat
+++ ERA/makeVPreview.bat
@@ -1,6 +1,8 @@
-@echo off
+rem @echo off
 @rem http://www.ffmpeg.org/download.html
 @rem 
-@echo %1 %2 %3 %4 >> premake1.log
+@echo %1 %2 %3 %4
 
-C:\Program Files\ffmpeg\bin\ffmpeg.exe C:\Erachains\IMG\demo1.mp4 dataPreviews\
+"C:\Program Files\ffmpeg\bin\ffmpeg.exe" -i C:\Erachains\IMG\demo1.mp4 -y -fs 200k -vcodec mpeg4 -q:v 20 -r:v 10 -s 255x255 dataPreviews\probe.mp4
+
+pause
