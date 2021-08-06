4e32ca10383770b6e30862d3a4abfaec7e1e81e7
icreator <icreator@mail.ru>
2021-08-06 14:01:59 +0300

    getProtocolBals

--- src/main/java/org/erachain/webserver/APIItemPerson.java
+++ src/main/java/org/erachain/webserver/APIItemPerson.java
@@ -14,7 +14,7 @@ import org.erachain.core.transaction.Transaction;
 import org.erachain.datachain.DCSet;
 import org.erachain.datachain.ItemPersonMap;
 import org.erachain.datachain.KKPersonStatusMap;
-import org.erachain.utils.NumberAsString;
+import org.erachain.datachain.TransactionFinalMap;
 import org.erachain.utils.StrJSonFine;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -57,6 +57,9 @@ public class APIItemPerson {
                 "Get Asset Key balance in Position [1..5] for Person Key. Balance Side =0 - total debit; =1 - left; =2 - total credit");
         help.put("GET apiperson/status/{personKey}/{statusKey}?history=true",
                 "Get Status data for Person Key. JSON ARRAY format: [timeFrom, timeTo, [par1, par2, str1, str2, reference, description], block, txNo]");
+
+        help.put("GET apiperson/protocolbals/{personKey}",
+                "Get all addresses for Person Key and protocol balances");
 
         help.put("GET apiperson/addresses/{key}?full", "Get Person Addresses");
         help.put("GET apiperson/statuses/{key}", "Get Person Statuses");
@@ -445,12 +448,19 @@ public class APIItemPerson {
         }
     }
 
+    /**
+     * ERA + COMPU
+     *
+     * @param key
+     * @return
+     */
     @GET
-    @Path("balances/{key}")
-    public Response getAppBals(@PathParam("key") Long key) {
+    @Path("protocolbals/{key}")
+    public Response getProtocolBals(@PathParam("key") Long key) {
 
         if (DCSet.getInstance().getItemPersonMap().get(key) == null) {
             JSONObject out = new JSONObject();
+            Transaction.updateMapByError2static(out, Transaction.ITEM_PERSON_NOT_EXIST, null);
             out.put("error", "Person not Found");
             return Response.status(200)
                     .header("Content-Type", "application/json; charset=utf-8")
@@ -459,9 +469,17 @@ public class APIItemPerson {
                     .build();
         }
 
-        TreeMap<String, Stack<Fun.Tuple3<Integer, Integer, Integer>>> addresses = DCSet.getInstance().getPersonAddressMap().getItems(person.getKey());
+        JSONObject output = new JSONObject();
+        TreeMap<String, Stack<Fun.Tuple3<Integer, Integer, Integer>>> addresses = DCSet.getInstance().getPersonAddressMap().getItems(key);
+        Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal,
+                BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balance;
+        JSONArray accountsArray = new JSONArray();
 
-        int i = 0;
+        TransactionFinalMap transactionsMap = DCSet.getInstance().getTransactionFinalMap();
+        BigDecimal eraBalanceA = BigDecimal.ZERO;
+        BigDecimal eraBalanceB = BigDecimal.ZERO;
+        BigDecimal compuBalance = BigDecimal.ZERO;
+
         for (String address : addresses.keySet()) {
 
             Stack<Fun.Tuple3<Integer, Integer, Integer>> stack = addresses.get(address);
@@ -469,55 +487,45 @@ public class APIItemPerson {
                 continue;
             }
 
-            Fun.Tuple3<Integer, Integer, Integer> item = stack.peek();
-            Transaction transactionIssue = transactionsMap.get(item.b, item.c);
-
             Map accountJSON = new LinkedHashMap();
             accountJSON.put("address", address);
-            accountJSON.put("to_date", item.a * 86400000l);
-            accountJSON.put("verifier", transactionIssue.getCreator().getAddress());
-            if (transactionIssue.getCreator().getPerson() != null) {
-                accountJSON.put("verifier_key", transactionIssue.getCreator().getPerson().b.getKey());
-                accountJSON.put("verifier_name", transactionIssue.getCreator().getPerson().b.viewName());
-            } else {
-                accountJSON.put("verifier_key", "");
-                accountJSON.put("verifier_name", "");
-            }
-
-            accountsJSON.put(i++, accountJSON);
 
             Account account = new Account(address);
-            List<Transaction> issuedPersons = transactionsMap.getTransactionsByAddressAndType(account.getShortAddressBytes(),
-                    Transaction.ISSUE_PERSON_TRANSACTION, 200, 0);
-            if (issuedPersons != null) {
-                myIssuePersons.addAll(issuedPersons);
-            }
 
-            Fun.Tuple5<Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>, Fun.Tuple2<BigDecimal, BigDecimal>> balance
-                    = account.getBalance(AssetCls.ERA_KEY);
-
+            balance = account.getBalance(AssetCls.ERA_KEY);
             eraBalanceA = eraBalanceA.add(balance.a.b);
             eraBalanceB = eraBalanceB.add(balance.b.b);
-            eraBalanceC = eraBalanceC.add(balance.c.b);
-            eraBalanceTotal = eraBalanceA.add(eraBalanceB).add(eraBalanceC);
 
             balance = account.getBalance(AssetCls.FEE_KEY);
             compuBalance = compuBalance.add(balance.a.b);
 
-            balance = account.getBalance(AssetCls.LIA_KEY);
-            liaBalanceA = liaBalanceA.add(balance.a.b);
-            liaBalanceB = liaBalanceB.add(balance.b.b);
+            accountJSON.put("asset1_balance_own", eraBalanceA);
+            accountJSON.put("asset1_balance_debt", eraBalanceB);
+            accountJSON.put("asset2_balance_own", compuBalance);
+
+            if (false) {
+                Fun.Tuple3<Integer, Integer, Integer> item = stack.peek();
+                Transaction transactionIssue = transactionsMap.get(item.b, item.c);
+
+                accountJSON.put("verified_to_date", item.a * 86400000L);
+                accountJSON.put("verifier", transactionIssue.getCreator().getAddress());
+                if (transactionIssue.getCreator().getPerson() != null) {
+                    accountJSON.put("verifier_key", transactionIssue.getCreator().getPerson().b.getKey());
+                    accountJSON.put("verifier_name", transactionIssue.getCreator().getPerson().b.viewName());
+                }
+            }
+
+            accountsArray.add(accountJSON);
+
         }
-        output.put("era_balance_a", NumberAsString.formatAsString(eraBalanceA));
-        output.put("era_balance_b", NumberAsString.formatAsString(eraBalanceB));
-        output.put("era_balance_c", NumberAsString.formatAsString(eraBalanceC));
-        output.put("era_balance_total", NumberAsString.formatAsString(eraBalanceTotal));
-        output.put("compu_balance", NumberAsString.formatAsString(compuBalance));
-        output.put("lia_balance_a", NumberAsString.formatAsString(liaBalanceA));
-        output.put("lia_balance_b", NumberAsString.formatAsString(liaBalanceB));
 
+        output.put("accounts", accountsArray);
 
-        output.put("accounts", accountsJSON);
+        return Response.status(200)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .header("Access-Control-Allow-Origin", "*")
+                .entity(output.toJSONString())
+                .build();
 
     }
 }
