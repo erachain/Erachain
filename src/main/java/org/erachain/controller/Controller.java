a269f89336009a7c934c05d416361a599461ff65
icreator <icreator@mail.ru>
2021-08-06 10:06:28 +0300

    vers 5.4.01

--- src/main/java/org/erachain/controller/Controller.java
+++ src/main/java/org/erachain/controller/Controller.java
@@ -100,8 +100,8 @@ import java.util.jar.Manifest;
  */
 public class Controller extends Observable {
 
-    public static String version = "5.4.1 dev";
-    public static String buildTime = "2021-05-05 12:00:00 UTC";
+    public static String version = "5.4.01";
+    public static String buildTime = "2021-08-05 12:00:00 UTC";
 
     public static final char DECIMAL_SEPARATOR = '.';
     public static final char GROUPING_SEPARATOR = '`';
