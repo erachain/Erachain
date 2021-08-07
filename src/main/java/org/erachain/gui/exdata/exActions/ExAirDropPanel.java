85524533c783729f0e80a91dce9fd6cbdc4448be
icreator <icreator@mail.ru>
2021-08-07 07:22:03 +0300

    lang mass payments

--- src/main/java/org/erachain/gui/exdata/exActions/ExAirDropPanel.java
+++ src/main/java/org/erachain/gui/exdata/exActions/ExAirDropPanel.java
@@ -361,7 +361,7 @@ public class ExAirDropPanel extends IconPanel implements ExActionPanelInt {
 
         int gridy = 0;
 
-        jCheckBoxAccrualsUse.setText(Lang.T("Make Air Drops"));
+        jCheckBoxAccrualsUse.setText(Lang.T("Make Payouts"));
         add(jCheckBoxAccrualsUse, fieldGBC);
 
         jLabel_Help.setText("<html>" + Lang.T("ExAirDropPanel_Help") + "</html>");
