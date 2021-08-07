07a3ef11ab02e62080d953e79eacd0e6d7a2ef91
icreator <icreator@mail.ru>
2021-08-07 23:15:48 +0300

    remove AssetUniqueSeries

--- src/main/java/org/erachain/gui/items/assets/AssetInfo.java
+++ src/main/java/org/erachain/gui/items/assets/AssetInfo.java
@@ -57,6 +57,9 @@ public class AssetInfo extends JTextPane {
     public AssetInfo(AssetCls asset, boolean fullView) {
         super();
 
+        if (asset == null)
+            return;
+
         try {
             //   initComponents();
             th = this;
