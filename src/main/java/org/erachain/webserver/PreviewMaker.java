153f7b1f31a1c73461633fd987d648206591e5eb
icreator <icreator@mail.ru>
2021-08-13 23:44:24 +0300

    getPreview for JPEG

--- src/main/java/org/erachain/webserver/PreviewMaker.java
+++ src/main/java/org/erachain/webserver/PreviewMaker.java
@@ -14,8 +14,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 
 public class PreviewMaker {
@@ -92,118 +90,116 @@ public class PreviewMaker {
         File outLog = new File(output);
         outLog.getParentFile().mkdirs();
 
-        if (!outLog.exists()) {
-            // иначе некоторые картинки дают сбой при конвертации в ffmpeg
-            pathIn += "." + mediaExt;
-            File fileIn = new File(pathIn);
-            try (FileOutputStream fos = new FileOutputStream(fileIn)) {
-                fos.write(image);
-            } catch (IOException e) {
-                LOGGER.error(e.getMessage(), e);
-                errorMess = e.getMessage();
-                return null;
-            }
+        if (outLog.exists())
+            return null;
 
-            ImageIcon imageIcon = null;
-            if (false) {
+        // иначе некоторые картинки дают сбой при конвертации в ffmpeg
+        pathIn += "." + mediaExt;
+        File fileIn = new File(pathIn);
+        try (FileOutputStream fos = new FileOutputStream(fileIn)) {
+            fos.write(image);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+            errorMess = e.getMessage();
+            return null;
+        }
+
+        try {
+            if (item.getImageType() == ItemCls.MEDIA_TYPE_IMG) {
+                ImageIcon imageIcon = new ImageIcon(pathIn);
+                BufferedImage bufferedImage = ImagesTools.imageToBufferedImage(imageIcon.getImage());
+                String[] props = bufferedImage.getPropertyNames();
+                int imageSize;
                 try {
-                    imageIcon = new ImageIcon(new URL("file", "", pathIn));
-                } catch (MalformedURLException e) {
+                    byte[] imageBytes = ImagesTools.bufferedImageToBytes(bufferedImage, mediaExt);
+                    imageSize = imageBytes.length;
+                } catch (IOException e) {
                     LOGGER.error(e.getMessage(), e);
                     errorMess = e.getMessage();
+                    return null;
                 }
-            } else {
-                imageIcon = new ImageIcon(pathIn);
-            }
+                float coefficient = (float) Math.sqrt((double) IMAGE_USE_ORIG_LEN / (double) imageSize);
+                int width = (int) (imageIcon.getIconWidth() * coefficient);
+                int height = (int) (imageIcon.getIconHeight() * coefficient);
 
-            BufferedImage bufferedImage = ImagesTools.imageToBufferedImage(imageIcon.getImage());
-            int imageSize;
-            try {
-                byte[] imageBytes = ImagesTools.bufferedImageToBytes(bufferedImage, mediaExt);
-                imageSize = imageBytes.length;
-            } catch (IOException e) {
-                LOGGER.error(e.getMessage(), e);
-                errorMess = e.getMessage();
-                return null;
-            }
-            float coefficient = (float) Math.sqrt((double) IMAGE_USE_ORIG_LEN / (double) imageSize);
-            int width = (int) (imageIcon.getIconWidth() * coefficient);
-            int height = (int) (imageIcon.getIconHeight() * coefficient);
-
-            try {
-                switch (mediaExt) {
-                    case "mp3":
-                        return null;
-                    case "jpg":
+                try {
+                    if (mediaExt.equals("gif")) {
                         imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
                         bufferedImage = ImagesTools.imageToBufferedImage(imageIcon.getImage());
-                        try {
-                            ImageIO.write(bufferedImage, "jpg", fileOut);
-                            return fileOut;
-                        } catch (IOException e) {
-                            LOGGER.error(e.getMessage(), e);
-                            errorMess = e.getMessage();
-                            return null;
-                        }
-
-                    case "mp4":
-                        String parQV;
-                        String parRV;
-                        if (image.length > 4000000) {
-                            parQV = "20";
-                            parRV = "10";
-                        } else if (image.length > 1500000) {
-                            parQV = "16";
-                            parRV = "12";
-                        } else if (image.length > 500000) {
-                            parQV = "14";
-                            parRV = "14";
-                        } else {
-                            parQV = "12";
-                            parRV = "15";
-                        }
-
-                        boolean isWindows = System.getProperty("os.name").startsWith("Windows");
-                        ProcessBuilder builder;
-                        // -i %1 -y -fs 512k -vcodec h264 -s 256x256 -q:v %2 -r:v %3 %4
-                        if (isWindows) {
-                            builder = new ProcessBuilder("makePreview.bat",
-                                    fileIn.toPath().toString(),
-                                    parQV, parRV,
-                                    fileOut.toPath().toString());
-                        } else {
-                            // в Unix через bash makePreview.bash - вызывает ошибку "Unable to find a suitable output format for"
-                            // - последний параметр как-то криво передается
-                            // -vf scale=256:-2,setsar=1:1
-                            builder = new ProcessBuilder("ffmpeg",
-                                    "-i", fileIn.toPath().toString(),
-                                    "-y", "-fs", "512k",
-                                    "-pix_fmt", "yuv420p", // for FireFox (neg error NS_ERROR_DOM_MEDIA_FATAL_ERR see https://github.com/ccrisan/motioneye/issues/1067
-                                    "-vcodec", "h264", "-an",
-                                    "-vf", "scale=256:-2,setsar=1:1", // "-s", "256x256",
-                                    "-q:v", parQV, "-r:v", parRV, fileOut.toPath().toString()
-                            );
-                        }
-                        // указываем перенаправление stderr в stdout, чтобы проще было отлаживать
-                        builder.redirectErrorStream(true);
-
-                        builder.redirectOutput(outLog);
-                        try {
-                            Process process = builder.start();
-                            process.waitFor();
-                            return fileOut;
-                        } catch (IOException | InterruptedException e) {
-                            LOGGER.error(e.getMessage(), e);
-                            errorMess = e.getMessage();
-                            return null;
-                        }
+                        ImageIO.write(bufferedImage, mediaExt, fileOut);
+                        return fileOut;
+                    } else {
+                        imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH));
+                        bufferedImage = ImagesTools.imageToBufferedImage(imageIcon.getImage());
+                        ImageIO.write(bufferedImage, mediaExt, fileOut);
+                        return fileOut;
+                    }
+                } catch (IOException e) {
+                    LOGGER.error(e.getMessage(), e);
+                    errorMess = e.getMessage();
+                    return null;
                 }
-            } finally {
-                if (fileOut.exists()) {
-                    fileIn.delete();
+
+            } else if (item.getImageType() == ItemCls.MEDIA_TYPE_VIDEO) {
+
+                String parQV;
+                String parRV;
+                if (image.length > 4000000) {
+                    parQV = "20";
+                    parRV = "10";
+                } else if (image.length > 1500000) {
+                    parQV = "16";
+                    parRV = "12";
+                } else if (image.length > 500000) {
+                    parQV = "14";
+                    parRV = "14";
+                } else {
+                    parQV = "12";
+                    parRV = "15";
+                }
+
+                boolean isWindows = System.getProperty("os.name").startsWith("Windows");
+                ProcessBuilder builder;
+                // -i %1 -y -fs 512k -vcodec h264 -s 256x256 -q:v %2 -r:v %3 %4
+                if (isWindows) {
+                    builder = new ProcessBuilder("makePreview.bat",
+                            fileIn.toPath().toString(),
+                            parQV, parRV,
+                            fileOut.toPath().toString());
+                } else {
+                    // в Unix через bash makePreview.bash - вызывает ошибку "Unable to find a suitable output format for"
+                    // - последний параметр как-то криво передается
+                    // -vf scale=256:-2,setsar=1:1
+                    builder = new ProcessBuilder("ffmpeg",
+                            "-i", fileIn.toPath().toString(),
+                            "-y", "-fs", "512k",
+                            "-pix_fmt", "yuv420p", // for FireFox (neg error NS_ERROR_DOM_MEDIA_FATAL_ERR see https://github.com/ccrisan/motioneye/issues/1067
+                            "-vcodec", "h264", "-an",
+                            "-vf", "scale=256:-2,setsar=1:1", // "-s", "256x256",
+                            "-q:v", parQV, "-r:v", parRV, fileOut.toPath().toString()
+                    );
+                }
+                // указываем перенаправление stderr в stdout, чтобы проще было отлаживать
+                builder.redirectErrorStream(true);
+
+                builder.redirectOutput(outLog);
+                try {
+                    Process process = builder.start();
+                    process.waitFor();
+                    return fileOut;
+                } catch (IOException | InterruptedException e) {
+                    LOGGER.error(e.getMessage(), e);
+                    errorMess = e.getMessage();
+                    return null;
                 }
 
             }
+
+        } finally {
+            if (fileOut.exists()) {
+                fileIn.delete();
+            }
+
         }
 
         // some errors was happen
