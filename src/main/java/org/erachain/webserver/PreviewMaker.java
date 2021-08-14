1c78d5dc03c02f60be4e08e107f7e3d0a0c5909f
icreator <icreator@mail.ru>
2021-08-14 09:49:09 +0300

    getPreview for animate GIF

--- src/main/java/org/erachain/webserver/PreviewMaker.java
+++ src/main/java/org/erachain/webserver/PreviewMaker.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.ws.rs.core.MediaType;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -33,6 +34,14 @@ public class PreviewMaker {
                 || item.getImageType() == AssetCls.MEDIA_TYPE_VIDEO && image.length < VIDEO_USE_ORIG_LEN
                 || item.getImageType() == AssetCls.MEDIA_TYPE_IMG && image.length < IMAGE_USE_ORIG_LEN;
 
+    }
+
+    public static MediaType getPreviewType(ItemCls item) {
+        String mediaExt = item.getImageTypeExt();
+        if (item.getImageType() == ItemCls.MEDIA_TYPE_IMG && mediaExt != "gif") {
+            return item.getImageMediaType();
+        }
+        return WebResource.TYPE_VIDEO;
     }
 
     public byte[] getPreview(ItemCls item, byte[] image) {
@@ -74,49 +83,35 @@ public class PreviewMaker {
         String mainFolder = "dataPreviews" + (BlockChain.DEMO_MODE ? "_demo" : BlockChain.TEST_MODE ? "_test" : "");
         String path = mainFolder + File.separator + outputName;
         String mediaExt = item.getImageTypeExt();
-        File fileOut = new File(path + "." + mediaExt);
-
-        fileOut.getParentFile().mkdirs();
-
-        if (fileOut.exists()) {
-            if (fileOut.canRead())
-                return fileOut;
-            // он еще записывается
-            return null;
-        }
 
         if (item.getImageType() == ItemCls.MEDIA_TYPE_IMG && mediaExt != "gif") {
-            // JPEG and PNG simple resize
-            ImageIcon imageIcon;
-            if (true) {
-                imageIcon = new ImageIcon(image, mediaExt);
-            } else {
-                String pathIn = mainFolder + File.separator + "orig" + File.separator + outputName;
-                pathIn += "." + mediaExt;
-                File fileIn = new File(pathIn);
-                try (FileOutputStream fos = new FileOutputStream(fileIn)) {
-                    fos.write(image);
-                } catch (IOException e) {
-                    LOGGER.error(e.getMessage(), e);
-                    errorMess = e.getMessage();
-                    return null;
-                }
-                imageIcon = new ImageIcon(pathIn);
-                if (fileOut.exists()) {
-                    fileIn.delete();
-                }
 
+            File fileOut = new File(path + "." + mediaExt);
+
+            fileOut.getParentFile().mkdirs();
+
+            if (fileOut.exists()) {
+                if (fileOut.canRead())
+                    return fileOut;
+                // он еще записывается
+                return null;
             }
-            //ImageIcon imageIcon = new ImageIcon(new URL(""));
+
+            // JPEG and PNG simple resize
+            ImageIcon imageIcon = new ImageIcon(image, mediaExt);
             float coefficient = (float) Math.sqrt((double) IMAGE_USE_ORIG_LEN / (double) image.length);
+            if (mediaExt == "jpg") {
+                coefficient *= 1.3;
+            } else if (mediaExt == "png") {
+                coefficient *= 0.8;
+            }
             int width = (int) (imageIcon.getIconWidth() * coefficient);
             int height = (int) (imageIcon.getIconHeight() * coefficient);
 
             BufferedImage bufferedImage;
             try {
                 imageIcon.setImage(imageIcon.getImage().getScaledInstance(width, height,
-                        //mediaExt == "png"? Image.SCALE_DEFAULT : Image.SCALE_SMOOTH
-                        Image.SCALE_DEFAULT
+                        mediaExt == "png" ? Image.SCALE_DEFAULT : Image.SCALE_SMOOTH
                 ));
                 bufferedImage = ImagesTools.imageToBufferedImage(imageIcon.getImage(),
                         mediaExt == "jpg" ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB);
@@ -130,6 +125,17 @@ public class PreviewMaker {
             }
 
         } else {
+
+            File fileOut = new File(path + ".mp4");
+
+            fileOut.getParentFile().mkdirs();
+
+            if (fileOut.exists()) {
+                if (fileOut.canRead())
+                    return fileOut;
+                // он еще записывается
+                return null;
+            }
 
             String parQV;
             String parRV;
