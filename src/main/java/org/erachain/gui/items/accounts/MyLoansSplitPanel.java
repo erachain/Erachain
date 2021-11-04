ff5ceee3bf6534435e1602a6475311fa09bff129
icreator <icreator@mail.ru>
2021-11-04 13:00:56 +0300

    MyLoansSplitPanel

--- src/main/java/org/erachain/gui/items/accounts/MyLoansSplitPanel.java
+++ src/main/java/org/erachain/gui/items/accounts/MyLoansSplitPanel.java
@@ -3,26 +3,31 @@ package org.erachain.gui.items.accounts;
 import org.erachain.core.account.Account;
 import org.erachain.core.item.assets.AssetCls;
 import org.erachain.gui.SplitPanel;
+import org.erachain.lang.Lang;
 
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 
-// панель моих адресов
+
 public class MyLoansSplitPanel extends SplitPanel {
 
-    public AccountsPanel accountPanel;
-    public AccountsRightPanel rightPanel;
+    private static final long serialVersionUID = 1L;
+    public LoansPanel loansPanel;
+    public AssetCls assetSelect;
+    private Account selectArg;
+    private AccountsRightPanel rightPanel;
 
     public static String NAME = "MyLoansSplitPanel";
     public static String TITLE = "My Loans";
 
     public MyLoansSplitPanel() {
         super(NAME, TITLE);
-        //	LayoutManager favoritesGBC = this.getLayout();
+
         this.jScrollPanelLeftPanel.setVisible(false);
         this.searchToolBar_LeftPanel.setVisible(false);
         this.toolBarLeftPanel.setVisible(false);
+        this.setName(Lang.T("My Loans"));
         this.jToolBarRightPanel.setVisible(false);
 
         GridBagConstraints PanelGBC = new GridBagConstraints();
@@ -32,38 +37,39 @@ public class MyLoansSplitPanel extends SplitPanel {
         PanelGBC.weighty = 1;
         PanelGBC.gridx = 0;
         PanelGBC.gridy = 0;
-
-        accountPanel = new AccountsPanel();
+        loansPanel = new LoansPanel();
+        this.leftPanel.add(loansPanel, PanelGBC);
+        // EVENTS on CURSOR
+        loansPanel.table.getSelectionModel().addListSelectionListener(new Account_Tab_Listener());
         rightPanel = new AccountsRightPanel();
 
-        this.leftPanel.add(accountPanel, PanelGBC);
-        //this.rightPanel1.add(rightPanel,PanelGBC);
-        jScrollPaneJPanelRightPanel.setViewportView(rightPanel);
-
-        // EVENTS on CURSOR
-        accountPanel.table.getSelectionModel().addListSelectionListener(new Account_Tab_Listener());
-
+        //   this.repaint();
 
     }
 
+    @Override
+    public void onClose() {
+        rightPanel.tableModel.deleteObservers();
+        loansPanel.tableModel.deleteObservers();
+    }
 
     class Account_Tab_Listener implements ListSelectionListener {
 
-        //@SuppressWarnings("deprecation")
         @Override
         public void valueChanged(ListSelectionEvent arg0) {
 
-            AssetCls asset = (AssetCls) accountPanel.cbxFavorites.getSelectedItem();
+            AssetCls asset = (AssetCls) loansPanel.cbxFavorites.getSelectedItem();
             Account account = null;
-            if (accountPanel.table.getSelectedRow() >= 0)
-                account = accountPanel.tableModel.getItem(accountPanel.table.convertRowIndexToModel(accountPanel.table.getSelectedRow()));
-            //info1.show_001(person);
-            //	rightPanel.jTable1.Search_Accoutnt_Transaction_From_Asset(account, asset);
-//			my_Accounts_SplitPanel.rightPanel.jTable1.revalidate();
-            // PersJSpline.setDividerLocation(PersJSpline.getDividerLocation());
-            //my_Person_SplitPanel.jSplitPanel.setDividerLocation(my_Person_SplitPanel.jSplitPanel.getDividerLocation());
-            ////my_Person_SplitPanel.searchTextFieldSearchToolBarLeftPanelDocument.setEnabled(true);
+            if (loansPanel.table.getSelectedRow() >= 0)
+                account = loansPanel.tableModel.getItem(loansPanel.table.convertRowIndexToModel(loansPanel.table.getSelectedRow()));
 
+            if (account == null || asset == null || account.equals(selectArg) && asset.equals(assetSelect)) return;
+            selectArg = account;
+            assetSelect = asset;
+            rightPanel.tableModel.setAccount(account);
+            rightPanel.tableModel.fireTableDataChanged();
+            rightPanel.setAsset(asset);
+            jScrollPaneJPanelRightPanel.setViewportView(rightPanel);
 
         }
 
--- src/main/java/org/erachain/gui/models/AccountLoansTableModel.java
+++ src/main/java/org/erachain/gui/models/AccountLoansTableModel.java
@@ -4,38 +4,32 @@ import org.erachain.controller.Controller;
 import org.erachain.core.account.Account;
 import org.erachain.core.account.PublicKeyAccount;
 import org.erachain.core.item.assets.AssetCls;
-import org.erachain.core.transaction.Transaction;
 import org.erachain.database.wallet.FavoriteAccountsMap;
 import org.erachain.gui.ObserverWaiter;
 import org.erachain.utils.NumberAsString;
 import org.erachain.utils.ObserverMessage;
 import org.mapdb.Fun;
 import org.mapdb.Fun.Tuple2;
-import org.mapdb.Fun.Tuple4;
 import org.mapdb.Fun.Tuple5;
 
 import java.math.BigDecimal;
 import java.util.Observable;
 
 @SuppressWarnings("serial")
-public class AccountsTableModel extends WalletTableModel<PublicKeyAccount> implements ObserverWaiter {
+public class AccountLoansTableModel extends WalletTableModel<PublicKeyAccount> implements ObserverWaiter {
+    public final int COLUMN_NO = 0;
     public static final int COLUMN_ADDRESS = 1;
     public static final int COLUMN_NAME = 2;
-    public static final int COLUMN_BALANCE_1 = 3;
-    public static final int COLUMN_BALANCE_2 = 4;
-    public static final int COLUMN_BALANCE_3 = 5;
-    public static final int COLUMN_BALANCE_4 = 6;
-    public static final int COLUMN_FEE_BALANCE = 7;
-    public final int COLUMN_NO = 0;
+    public static final int COLUMN_SIDE_ADDRESS = 3;
+    public static final int COLUMN_SIDE_NAME = 4;
+    public static final int COLUMN_BALANCE_2 = 5;
     private AssetCls asset;
     private Long assetKey;
 
-    public AccountsTableModel() {
+    public AccountLoansTableModel() {
         super(Controller.getInstance().getWallet().dwSet.getAccountMap(),
-                new String[]{"No.", "Account", "Name",
-                        "OWN (1)", "DEBT (2)", "HOLD (3)", "SPEND (4)",
-                        AssetCls.FEE_NAME},
-                new Boolean[]{true, false, false, false, false, false, false, false}, false, -1000);
+                new String[]{"No.", "Account", "Name", "Side Account", "Name", "DEBT or LOAN"},
+                new Boolean[]{true, false, false, false, false}, false, -1000);
 
         getInterval();
         fireTableDataChanged();
@@ -87,54 +81,42 @@ public class AccountsTableModel extends WalletTableModel<PublicKeyAccount> imple
                 }
                 return "";
 
-            case COLUMN_BALANCE_1:
-                if (this.asset == null) return "-";
-                balance = account.getBalance(assetKey);
-                str = NumberAsString.formatAsString(balance.a.b.setScale(asset.getScale()));
-                return str;
             case COLUMN_BALANCE_2:
                 if (this.asset == null) return "-";
                 balance = account.getBalance(assetKey);
                 str = NumberAsString.formatAsString(balance.b.b.setScale(asset.getScale()));
                 return str;
-            case COLUMN_BALANCE_3:
-                if (this.asset == null) return "-";
-                balance = account.getBalance(assetKey);
-                str = NumberAsString.formatAsString(balance.c.b.setScale(asset.getScale()));
-                return str;
-            case COLUMN_BALANCE_4:
-                if (this.asset == null) return "-";
-                balance = account.getBalance(assetKey);
-                str = NumberAsString.formatAsString(balance.d.b.setScale(asset.getScale()));
-                return str;
-            case COLUMN_FEE_BALANCE:
-                return NumberAsString.formatAsString(account.getBalance(Transaction.FEE_KEY).a.b.stripTrailingZeros());
+            case COLUMN_SIDE_ADDRESS:
+                return account.getPersonAsString();
+
+            case COLUMN_SIDE_NAME:
+                favoriteMap = Controller.getInstance().getWallet().dwSet.getFavoriteAccountsMap();
+                if (favoriteMap.contains(account.getAddress())) {
+                    Fun.Tuple3<String, String, String> itemAccount = favoriteMap.get(account.getAddress());
+                    if (itemAccount.b != null)
+                        return itemAccount.b;
+
+                }
         }
 
         return null;
     }
 
-    public Tuple4<BigDecimal, BigDecimal, BigDecimal, BigDecimal> getTotalBalance() {
+    public BigDecimal getTotalBalance() {
 
         if (this.asset == null || list == null)
-            return new Tuple4(BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
+            return BigDecimal.ZERO;
 
-        BigDecimal totalBalance1 = BigDecimal.ZERO;
         BigDecimal totalBalance2 = BigDecimal.ZERO;
-        BigDecimal totalBalance3 = BigDecimal.ZERO;
-        BigDecimal totalBalance4 = BigDecimal.ZERO;
 
         Tuple5<Tuple2<BigDecimal, BigDecimal>, Tuple2<BigDecimal, BigDecimal>, Tuple2<BigDecimal, BigDecimal>, Tuple2<BigDecimal, BigDecimal>, Tuple2<BigDecimal, BigDecimal>> balance;
         for (Account account : this.list) {
             balance = account.getBalance(assetKey);
 
-            totalBalance1 = totalBalance1.add(balance.a.b);
             totalBalance2 = totalBalance2.add(balance.b.b);
-            totalBalance3 = totalBalance3.add(balance.c.b);
-            totalBalance4 = totalBalance4.add(balance.d.b);
         }
 
-        return new Tuple4(totalBalance1, totalBalance2, totalBalance3, totalBalance4);
+        return totalBalance2;
     }
 
     public void syncUpdate(Observable o, Object arg) {
