d4b81ca8caf1c933165d4fb14be6ffa943fe7494
icreator <icreator@mail.ru>
2022-05-27 10:03:05 +0300

    AS_OUTSIDE_BILL_4

--- src/main/java/org/erachain/core/item/assets/AssetCls.java
+++ src/main/java/org/erachain/core/item/assets/AssetCls.java
@@ -148,14 +148,14 @@ public abstract class AssetCls extends ItemCls {
     public static final int AS_OUTSIDE_SHARE = 13;
 
     /**
-     * outside BILL - вексель
+     * outside BILL - вексель - promissory note
      * +++ вексель на оплату во вне
      * === полный аналог OUTSIDE_CLAIM по действиям в протоколе - чисто для наименования другого
      */
     public static final int AS_OUTSIDE_BILL = 14;
 
     /**
-     * outside BILL - вексель
+     * outside BILL - вексель переводной (тратта) - bill of exchange
      * +++ вексель на оплату во вне
      * === полный аналог OUTSIDE_CLAIM по действиям в протоколе - чисто для наименования другого
      */
@@ -400,6 +400,7 @@ public abstract class AssetCls extends ItemCls {
                 AS_OUTSIDE_CURRENCY,
                 AS_OUTSIDE_SERVICE,
                 AS_OUTSIDE_BILL,
+                AS_OUTSIDE_BILL_EX,
                 AS_OUTSIDE_WORK_TIME_HOURS,
                 AS_OUTSIDE_WORK_TIME_MINUTES,
                 AS_OUTSIDE_SHARE,
@@ -506,6 +507,8 @@ public abstract class AssetCls extends ItemCls {
             case AS_INSIDE_VOTE:
                 return "✋";
             case AS_OUTSIDE_BILL:
+                return "⬖"; // ⬒
+            case AS_OUTSIDE_BILL_EX:
                 return "⬖"; // ⬒
             case AS_OUTSIDE_SERVICE:
                 return "⬔";
@@ -838,7 +841,7 @@ public abstract class AssetCls extends ItemCls {
     }
 
     public boolean isOutsideBill() {
-        return this.assetType == AS_OUTSIDE_BILL;
+        return this.assetType == AS_OUTSIDE_BILL || this.assetType == AS_OUTSIDE_BILL_EX;
     }
 
     public boolean isOutsideBillEx() {
@@ -1069,7 +1072,7 @@ public abstract class AssetCls extends ItemCls {
             case AS_OUTSIDE_BILL:
                 return "AS_OUTSIDE_BILL_N";
             case AS_OUTSIDE_BILL_EX:
-                return "Bill of exchange";
+                return "AS_OUTSIDE_BILL_EX_N";
             case AS_MY_DEBT:
                 return "AS_MY_DEBT_N";
             case AS_OUTSIDE_OTHER_CLAIM:
@@ -1142,7 +1145,7 @@ public abstract class AssetCls extends ItemCls {
             case AS_OUTSIDE_BILL:
                 return "AS_OUTSIDE_BILL_NF";
             case AS_OUTSIDE_BILL_EX:
-                return "Bill of Exchange";
+                return "AS_OUTSIDE_BILL_EX_NF";
             case AS_MY_DEBT:
                 return "AS_MY_DEBT_NF";
             case AS_OUTSIDE_OTHER_CLAIM:
@@ -1295,7 +1298,7 @@ public abstract class AssetCls extends ItemCls {
             case AS_OUTSIDE_BILL:
                 return "AS_OUTSIDE_BILL_D";
             case AS_OUTSIDE_BILL_EX:
-                return "A digital bill of exchange can be called for redemption by external money. You can take it into your hands";
+                return "AS_OUTSIDE_BILL_EX_D";
             case AS_MY_DEBT:
                 return "AS_MY_DEBT_D";
             case AS_OUTSIDE_OTHER_CLAIM:
@@ -1436,7 +1439,6 @@ public abstract class AssetCls extends ItemCls {
                         return null;
                 }
             case AS_OUTSIDE_BILL:
-            case AS_OUTSIDE_BILL_EX:
                 switch (actionType) {
                     case Account.BALANCE_POS_OWN:
                         return backward ? null : "AS_OUTSIDE_BILL_1";
@@ -1445,6 +1447,18 @@ public abstract class AssetCls extends ItemCls {
                                 : "AS_OUTSIDE_BILL_2";
                     case Account.BALANCE_POS_SPEND:
                         return backward ? null : "AS_OUTSIDE_BILL_4";
+                    default:
+                        return null;
+                }
+            case AS_OUTSIDE_BILL_EX:
+                switch (actionType) {
+                    case Account.BALANCE_POS_OWN:
+                        return backward ? null : "AS_OUTSIDE_BILL_EX_1";
+                    case Account.BALANCE_POS_DEBT:
+                        return backward ? "AS_OUTSIDE_BILL_EX_2B"
+                                : "AS_OUTSIDE_BILL_EX_2";
+                    case Account.BALANCE_POS_SPEND:
+                        return backward ? null : "AS_OUTSIDE_BILL_EX_4";
                     default:
                         return null;
                 }
